3acf57dcf46d7ef886e1ccf632701a21eed81fab
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-22 14:55:05 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.14 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        String [] a = new String[n];
+        for (int i=0;i<n;i++) {
+            if (i % 2 == 0) {
+                a[i] = "수";
+            }
+            else {
+                a[i] = "박";
+            }
+        }
+        answer = String.join("", a);
+        return answer;
+    }
+}

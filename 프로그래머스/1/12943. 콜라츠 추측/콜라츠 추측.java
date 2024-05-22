971d69a7cfd54bd3f645e66280edd2b220598e07
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-22 22:16:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,23 +2,20 @@ class Solution {
     public int solution(int num) {
         int answer = 0;
         int count = 0;
-        while(num != 1) { // 1이 아닌 경우에만 루프 실행
-            for (count = 1; count < 500; count++) {
-                if (num % 2 == 0) {
-                    num /= 2;
-                } 
-                else {
-                    num = num * 3 + 1;
-                }
-                if (num == 1) { // 1에 도달하면 루프 종료
-                    break;
-                }
+        // num이 1이 아닐 때까지 반복
+        while(num!=1) {
+            if (num % 2 == 0) {
+                num /= 2;
             }
-            if (count >= 400) { // 500회 반복 시도 후에도 1에 도달하지 못한 경우 -1 반환
+            else {
+                num = (num*3) + 1;
+            }
+            count++;
+         // 400회 이상이면 -1 리턴
+            if (count > 400) {
                 return -1;
             }
         }
         return count;
-        
     }
 }

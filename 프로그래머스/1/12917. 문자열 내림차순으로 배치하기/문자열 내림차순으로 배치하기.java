ad56c7d0e9b058978150d411fef8e8333b5b11d3
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-22 22:01:31 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.44 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -1,20 +1,27 @@
 import java.util.*;
 class Solution {
     public String solution(String s) {
-        Character[] chars = new Character[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            chars[i] = s.charAt(i);
+        String answer = "";
+        // 문자배열 생성
+        Character[] a = new Character[s.length()];
+        
+        // a에 저장
+        for (int i = 0;i<s.length();i++) {
+            a[i] = s.charAt(i);
         }
-
-        // 배열을 내림차순으로 정렬
-        Arrays.sort(chars, Collections.reverseOrder());
-
-        // 정렬된 배열을 다시 문자열로 변환
-        StringBuilder sb = new StringBuilder(chars.length);
-        for (char c : chars) {
-            sb.append(c);
+        
+        // 내림차순 sort
+        Arrays.sort(a,Collections.reverseOrder());
+        
+        // StringBuilder b 생성
+        StringBuilder b = new StringBuilder(s.length());
+        
+        // b에 저장
+        for (char c:a) {
+            b.append(c);
         }
-
-        return sb.toString();
+        
+        // 문자열로 변환
+        return b.toString();
     }
 }

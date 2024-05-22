9bc8d4be71bc8cb087c91b962dd6f36befe73744
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-23 02:08:24 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,29 +1,29 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer = new int[arr.length-1];
-        int index = 0;
         
-        //배열의 길이가 1일 때 배열 -1 리턴
+        // 배열 길이가 1이면 -1 리턴
         if (arr.length == 1) {
             return new int[]{-1};
         }
         
-        //min변수에 최솟값 넣기
-        int min = arr[0];
-        for (int i : arr) {
-            if (min > i) {
-                min = i;
+        // 제일 작은 수 찾기(버블정렬?)
+        int num = arr[0];
+        for (int i = 0;i<arr.length;i++) {
+            if (num > arr[i]) {
+                num = arr[i];
             }
         }
         
-        //최솟값이 아닌 값을 answer에 저장
-        for (int j : arr) {
-            if (j != min) {
-                answer[index] = j;
+        // 제일 작은 수 num과 같지 않은 수를 배열에 넣기
+        int index = 0;
+        for (int a:arr) {
+            if (a != num) {
+                answer[index] = a;
                 index++;
             }
         }
-        
         return answer;
     }
 }

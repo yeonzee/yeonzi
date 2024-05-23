ec3b6a42bbe310e82ef3be7764379c0b95d80a91
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-23 15:39:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 50.74 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int num : arr) {
+            // 스택이 비어있지 않고, 현재 숫자가 스택의 맨 위 숫자와 같으면 스택에서 제거하지 않음
+            if (!stack.isEmpty() && stack.peek() == num) {
+                continue;
+            }
+            else {
+                stack.push(num);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        for (int i =stack.size()-1;i>=0;i--) {
+            answer[i] = stack.pop();
+        }
+        return answer;
+    }
+}

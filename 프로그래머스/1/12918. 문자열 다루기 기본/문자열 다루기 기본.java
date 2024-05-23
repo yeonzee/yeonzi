7d81d9cd8202ab3c6135e56210b0cf54a5434c90
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-23 13:28:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,8 +1,13 @@
 class Solution {
     public boolean solution(String s) {
-        if(s.length() != 4 && s.length() != 6) return false;
+        if(s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        
         for (int i=0;i<s.length();i++) {
-            if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
+            //아스키코드로 변환해서 숫자가 아닌 것들
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') 
+                return false;
         }        
         return true;
     }

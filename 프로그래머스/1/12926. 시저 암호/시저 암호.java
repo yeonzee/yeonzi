a735762cbb1eb930323498bfffb64ddad27e679d
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-24 22:43:25 +0900

    [level 1] Title: 시저 암호, Time: 29.26 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,23 @@
 class Solution {
     public String solution(String s, int n) {
-        StringBuilder encrypted = new StringBuilder();
+        String answer = "";
         
-        for (char c : s.toCharArray()) {
-            if (Character.isLowerCase(c)) {
-                char encryptedChar = (char) (((c - 'a' + n) % 26) + 'a');
-                encrypted.append(encryptedChar);
-            } else if (Character.isUpperCase(c)) {
-                char encryptedChar = (char) (((c - 'A' + n) % 26) + 'A');
-                encrypted.append(encryptedChar);
-            } else {
-                // 공백 등의 문자는 그대로 유지
-                encrypted.append(c);
+        //n이 26을 초과하여도 알파벳을 한 바퀴 돌려서 계산
+        n = n % 26;
+        
+        
+        for (int i = 0; i < s.length(); i++) {
+        char ch = s.charAt(i);
+            //소문자
+            if (Character.isLowerCase(ch)) {
+                ch = (char) ((ch - 'a' + n) % 26 + 'a');
             }
+            //대문자
+            else if (Character.isUpperCase(ch)) {
+                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+            }
+            answer += ch;
         }
-        
-        return encrypted.toString();
+        return answer;
     }
 }

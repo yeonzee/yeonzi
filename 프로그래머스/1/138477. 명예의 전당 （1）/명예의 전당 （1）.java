c59a5b1ce95505379810de64c9ddb5a1365e03f8
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-27 17:44:41 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.47 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,22 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        
-        ArrayList<Integer> arr = new ArrayList(); 
 
-        for(int i=0; i < score.length; i++) {
-            if(i < k) {
-                arr.add(score[i]);
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int temp = 0;
+
+        for(int i = 0; i < score.length; i++) {
+
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
             }
-            else {
-                if(score[i] >= arr.get(0)){
-                    arr.remove(0);
-                    arr.add(score[i]);
-                }
-            }
-            Collections.sort(arr);
-            answer[i] = arr.get(0);
+
+            answer[i] = priorityQueue.peek();
         }
+
+
+
         return answer;
     }
 }

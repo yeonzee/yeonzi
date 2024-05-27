a1f4774ecb939c5bd1a71cf3e8c2a6f6aa32cdaf
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-28 02:23:18 +0900

    [level 1] Title: 추억 점수, Time: 0.04 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,22 +1,24 @@
+//HashMap을 사용해야 한다고 생각
 import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        
         HashMap<String, Integer> map = new HashMap<String, Integer>();
         
+        //name과 yearning의 각각의 값 할당
         for (int i=0;i<yearning.length;i++) {
-            map.put(name[i],yearning[i]);
+            map.put(name[i], yearning[i]);
         }
-        
         
         for (int j=0;j<photo.length;j++) {
             int sum = 0;
             for (int k=0;k<photo[j].length;k++) {
-                Integer value = map.get(photo[j][k]);
-                if (value != null) {
-                    sum += map.get(photo[j][k]);
-                } 
+                //map에 photo[j][k]에 해당하는 값의 value 값
+                Integer num = map.get(photo[j][k]);
+                
+                if (num != null) {
+                    sum += num;
+                }
             }
             answer[j] = sum;
         }

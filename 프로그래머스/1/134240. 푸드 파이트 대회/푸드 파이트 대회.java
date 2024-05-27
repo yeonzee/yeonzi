6eba83117f05076d12507f545d783042b332e296
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-27 23:10:02 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.86 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -5,29 +5,14 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         for (int i=1;i<food.length;i++) {
-            if(food[i]>1) {
-                sb.append(String.valueOf(i).repeat(food[i]/2));
+            if (food[i] > 1) {
+                sb.append(String.valueOf(i).repeat(food[i] / 2));
             }
             else {
                 continue;
             }
-        }
-        
-        String[] str = new String[sb.length()];
-        for (int j = 0; j < sb.length(); j++) {
-            str[j] = String.valueOf(sb.charAt(j));
-        }
-        
-        
-        
-        String result = sb.toString() + "0";
-        
-        Arrays.sort(str, Collections.reverseOrder());
-        
-        for (String s : str) {
-            result += s;
-        }
-        
-        return result;
+        }  
+        answer = sb + "0";
+        return answer + sb.reverse();
     }
 }

1942a9967a1700ce8c1f684c9bd0e54131d4111b
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-28 01:51:28 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.35 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,25 +1,22 @@
+//우선순위 큐 사용
 import java.util.*;
-
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-
+        
+        //우선순위 큐는 작은 것을 반환
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-
-        int temp = 0;
-
-        for(int i = 0; i < score.length; i++) {
-
+        
+        for (int i=0;i<score.length;i++) {
             priorityQueue.add(score[i]);
+            
             if (priorityQueue.size() > k) {
                 priorityQueue.poll();
+                //첫번째 값 반환한 후 제거!! 비어있으면 null반환
             }
-
+            //첫번째 값 반환
             answer[i] = priorityQueue.peek();
         }
-
-
-
         return answer;
     }
 }

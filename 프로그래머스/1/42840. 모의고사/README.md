35ff362b300f792b120e86a3f5e2240602434512
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-28 18:14:13 +0900

    [level 1] Title: 모의고사, Time: 0.66 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 81.3 MB, 시간: 0.76 ms
+메모리: 72.5 MB, 시간: 0.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 28일 17:51:37
+2024년 05월 28일 18:14:10
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,45 +1,48 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        List<Integer> test = new ArrayList<>();
-        int[] last= new int[3];
-        //참가자들
-        int[] one={1,2,3,4,5};
-        int[] two={2,1,2,3,2,4,2,5};
-        int[] thr={3,3,1,1,2,2,4,4,5,5};
-        // 세팅 종료
-
-        //맞춘 개수 세팅
-        for(int i=0;i<answers.length;i++){
+        //3명의 수포자 정답의 개수를 담을 배열
+        int[] list = new int[3];
+        
+        //각 수포자의 답이 담긴 배열
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] thr = {3,3,1,1,2,2,4,4,5,5};
+        
+        //answers[i]의 값이 1,2,3번의 수포자들의 정답과 일치한지 비교
+        //배열.length로 나누어 나머지 값을 인덱스로 하는 값 반환
+        //else if가 아닌 if로 해야 함!!
+        for (int i=0;i<answers.length;i++) {
             if(answers[i]==one[i%one.length]) {
-                last[0]++;
+                list[0]++;
             }
             if(answers[i]==two[i%two.length]) {
-                last[1]++;
+                list[1]++;
             }
             if(answers[i]==thr[i%thr.length]) {
-                last[2]++;
+                list[2]++;
             }
         }
-
-        //최대값 세팅
+ 
+        //최대값 뽑기
         int max=0;
-        for(int i:last) {
-            max=Math.max(i,max);
+        for (int j:list) {
+            max = Math.max(j,max);
         }
-
-        //최대로 맞춘 사람 정리
-        for(int i=0;i<3;i++){
-            if(last[i]==max){
-                test.add(i+1);
+        
+        //max값과 일치한 인덱스(수포자 번호)를 arr리스트에 저장
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int k=0;k<3;k++) {
+            if(list[k]==max) {
+                arr.add(k+1);
             }
         }
-        int size= test.size();
-        int[] answer=new int[size];
-        for(int i=0;i<size;i++) {
-            answer[i]=test.get(i);
+        //배열로 변환
+        int[] answer = new int[arr.size()];
+        for (int num=0;num<arr.size();num++) {
+            answer[num] = arr.get(num);
         }
+        
         return answer;
     }
 }

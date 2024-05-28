e63f9299040abd206b5087dd75c22c10d29d925c
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-28 15:23:40 +0900

    [level 1] Title: 폰켓몬, Time: 3.34 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,14 @@
-//
-
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         ArrayList<Integer> arr = new ArrayList<>();
         for (int num:nums) {
+            //arr에 num이 포함되어 있지 않다면 arr에 num 추가
             if (!arr.contains(num)) {
                 arr.add(num);
             }
+            //arr사이즈가 n/2보다 크거나 같다면 break
             if (arr.size() >= (nums.length / 2)) {
                 break;
             }

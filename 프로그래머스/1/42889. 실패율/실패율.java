74bb9767aa37fdc9303f4ab91458d746d21f6827
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-30 01:53:55 +0900

    [level 1] Title: 실패율, Time: 2.68 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,35 +1,38 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int N, int[] stages) {
-        HashMap<Integer, Double> map = new HashMap<>();
-        int[] userFailCnts = new int[N+2];
-        int[] userTotalCnts = new int[N+1];
-        
-        //스테이지 별 머물러있는 사용자 수 카운트
-        for(int stage : stages) {
-            userFailCnts[stage]++;
+    	// 스테이지별 도달했으나 클리어하지 못한 사람 수
+        int[] currentStages = new int[N+1];
+        // 스테이지별 도달한 사람 수
+        int[] clearStages = new int[N+1];
+
+        for (int i = 0; i < stages.length; i++) {
+            // 스테이지에 도달한 플레이어 수 증가
+            for (int j = 0; j < stages[i]; j++) {
+                clearStages[j] += 1;
+            }
+            // 도달 했으나 클리어하지 못한 플레이어 수 증가
+            currentStages[stages[i] - 1] += 1;
         }
-        
-        //스테이지 별 도달한 플레이어 수 카운트
-        userTotalCnts[N] = userFailCnts[N] + userFailCnts[N+1];       
-        for(int i = N-1; i >=1; i--) {
-            userTotalCnts[i] = userFailCnts[i] + userTotalCnts[i+1];
-        }
-        
-        //스테이지 별 실패율 계산
-        for (int i = 1; i < userTotalCnts.length; i++) {
-            if (userFailCnts[i] == 0 || userTotalCnts[i] == 0) {
-                map.put(i, 0.0);
+
+        // 스테이지 번호와 실패율을 저장할 map
+        Map<Integer, Double> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            // 스테이지에 도달한 사람이 없거나 클리어한 사람이 없는 경우
+            if (currentStages[i] == 0 || clearStages[i] == 0) {
+                map.put(i + 1, 0.0);
             } else {
-                map.put(i, (double)userFailCnts[i] / userTotalCnts[i]);
+                // 실패율 계산하여 맵에 저장
+                map.put(i + 1, (double) currentStages[i] / (double) clearStages[i]);
             }
         }
-        
-        //실패율(value) 값으로 스테이지(key)를 내림차순 정렬
-        List<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, (o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
 
-        return list.stream().mapToInt(Integer::intValue).toArray(); 
-    } 
+        List<Integer> list = new ArrayList<>(map.keySet());
+        // 실패율을 기준으로 내림차순 정렬
+        list.sort((o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
+
+        // 리스트를 배열로 변환하여 반환
+        return list.stream().mapToInt(i -> i).toArray();
+    }
 }

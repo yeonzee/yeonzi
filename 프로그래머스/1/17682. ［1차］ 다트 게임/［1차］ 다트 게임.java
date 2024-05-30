04b6b699549ca4a21165b53131f2cc5a0454ebe5
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-30 15:06:06 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,42 +1,48 @@
 import java.util.*;
-
 class Solution {
     public int solution(String dartResult) {
-        int[] scores = new int[3];
-        int index = -1;  // 현재 점수의 인덱스
+        int answer = 0;
+        int[] result = new int[3];
+        int index = -1;
         
-        for (int i = 0; i < dartResult.length(); i++) {
+        for(int i=0;i<dartResult.length();i++) {
             char c = dartResult.charAt(i);
             
             if (Character.isDigit(c)) {
-                // 숫자가 10일 경우를 처리
-                if (c == '1' && dartResult.charAt(i + 1) == '0') {
-                    scores[++index] = 10;
-                    i++;  // '0'을 건너뛴다
-                } else {
-                    scores[++index] = c - '0';
+                if (c == '1' && dartResult.charAt(i+1) == '0') {
+                    result[++index] = 10;
+                    i++; //0을 건너뛴다...!!!!
                 }
-            } else if (c == 'S') {
-                scores[index] = (int) Math.pow(scores[index], 1);
-            } else if (c == 'D') {
-                scores[index] = (int) Math.pow(scores[index], 2);
-            } else if (c == 'T') {
-                scores[index] = (int) Math.pow(scores[index], 3);
-            } else if (c == '*') {
-                if (index > 0) { //이전 턴이 존재할 경우
-                    scores[index - 1] *= 2; //이전 턴 점수 2배
+                else {
+                    result[++index] = c - '0';
                 }
-                scores[index] *= 2; //현재 턴 점수 2배
-                
-            } else if (c == '#') {
-                scores[index] *= -1;
             }
-        }
-        int answer = 0;
-        for (int score : scores) {
-            answer += score;
+                
+            else if (c == 'S') {
+                result[index] = (int)Math.pow(result[index],1);
+            }
+            else if (c == 'D') {
+                result[index] = (int)Math.pow(result[index],2);
+            }
+            else if (c == 'T') {
+                result[index] = (int)Math.pow(result[index],3);
+            }
+
+            else if (c == '*') {
+                if (index > 0) { //이전 턴 존재
+                    result[index-1] *= 2;
+                }
+                result[index] *= 2;
+            }
+
+            else if (c == '#') {
+                result[index] *= -1;
+            }  
         }
         
+        for (int num:result) {
+            answer += num;
+        }
         return answer;
     }
 }

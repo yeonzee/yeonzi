0fcfee306325a0312400173f1b1d5f065bc85d5b
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-05-31 15:32:01 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.40 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,8 +1,6 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
-        int[] result = new int[2];
         
         Arrays.sort(lottos);
         Arrays.sort(win_nums);
@@ -22,46 +20,8 @@ class Solution {
                 
             }
         }
-        
-        if (count_good == 6) {
-            result[0] = 1;
-        }
-        else if (count_good == 5) {
-            result[0] = 2;
-        }
-        else if (count_good == 4) {
-            result[0] = 3;
-        }
-        else if (count_good == 3) {
-            result[0] = 4;
-        }
-        else if (count_good == 2) {
-            result[0] = 5;
-        }
-        else {
-            result[0] = 6;
-        }
-        
-        if (count_bad == 6) {
-            result[1] = 1;
-        }
-        else if (count_bad == 5) {
-            result[1] = 2;
-        }
-        else if (count_bad == 4) {
-            result[1] = 3;
-        }
-        else if (count_bad == 3) {
-            result[1] = 4;
-        }
-        else if (count_bad == 2) {
-            result[1] = 5;
-        }
-        else {
-            result[1] = 6;
-        }
-        
-        
-        return result;
+        int[] answer = {Math.min(7 - count_good, 6), Math.min(7 - count_bad, 6)};
+        return answer;
+
     }
 }

5626abb8081b2651f7209873e2a8681ac6ff2ce9
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-06-03 18:21:24 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,23 +1,28 @@
+/*
+리스트에 ingredient를 하나씩 넣은 후 4개가 됐을 때부터 역순으로 조사
+1321이 있으면 +1, 리스트에서 제거
+*/
+
 import java.util.*;
 class Solution {
     public int solution(int[] ingredient) {
-       int answer = 0;
-		ArrayList<Integer> li = new ArrayList<>();
-		
-		for(int i :ingredient) {
-			li.add(i);
-			while(li.size() >= 4) {
-				int n = li.size();
-				if(!(li.get(n-1) == 1
-					&& li.get(n-2)==3
-					&& li.get(n-3) ==2
-					&& li.get(n-4)==1)) break;
-				for(int j=0; j<4; j++) {
-					li.remove(li.size() -1);
-				}
-				answer++;
-			}
-		}
-        return answer;
+        int answer = 0;
+        ArrayList<Integer> li = new ArrayList<>();
+        int count = 0;
+        for (int a:ingredient) {
+            li.add(a);
+            if (li.size()>=4 
+                 && li.get(li.size()-1)==1 
+                 && li.get(li.size()-2)==3
+                 && li.get(li.size()-3)==2
+                 && li.get(li.size()-4)==1) {
+                count++;
+
+                for (int j=0;j<4;j++) {
+                    li.remove(li.size()-1);
+                }
+            }
+        }
+        return count;
     }
 }

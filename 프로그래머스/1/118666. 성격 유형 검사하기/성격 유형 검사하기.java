6982c855c0743022a54f3e07d0256de4da36972a
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-06-05 17:33:10 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.91 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,27 +1,49 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] survey, int[] choices) {
-        Map<Character, Integer> map = new HashMap<>();
-
-        for(int i = 0; i< survey.length; i++) {
-            int value = choices[i];
-
-            if(value > 0 && value < 4) {
-                char ch = survey[i].charAt(0);
-                map.put(ch, map.getOrDefault(ch, 0) + 4 - value);
-            } else if(value > 4) {
-                char ch = survey[i].charAt(1);
-                map.put(ch, map.getOrDefault(ch, 0) + value - 4);
+        String answer = "";
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for (int i=0; i<survey.length;i++) {
+            int n = choices[i];
+            if(n < 4) { //n = 1,2,3
+                char c = survey[i].charAt(0);
+                map.put(c, map.getOrDefault(c,0)+4-n);
             }
-
+            else if (n > 4) { //n = 5,6,7
+                char c = survey[i].charAt(1);
+                map.put(c,map.getOrDefault(c,0)+n-4);
+            }
         }
-
-        return new StringBuilder()
-            .append(map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? 'R' : 'T')
-            .append(map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? 'C' : 'F')
-            .append(map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? 'J' : 'M')
-            .append(map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? 'A' : 'N')
-            .toString();
+        
+        StringBuilder sb = new StringBuilder();
+        if (map.getOrDefault('R',0) >= map.getOrDefault('T',0)) {
+            sb.append('R');
+        }
+        else {
+            sb.append('T');
+        }
+        
+        if (map.getOrDefault('C',0) >= map.getOrDefault('F',0)) {
+            sb.append('C');
+        }
+        else {
+            sb.append('F');
+        }
+        if (map.getOrDefault('J',0) >= map.getOrDefault('M',0)) {
+            sb.append('J');
+        }
+        else {
+            sb.append('M');
+        }
+        if (map.getOrDefault('A',0) >= map.getOrDefault('N',0)) {
+            sb.append('A');
+        }
+        else {
+            sb.append('N');
+        }
+        
+        
+        return sb.toString();
     }
 }

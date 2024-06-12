eb0efc87eff1f64a2e9b785f2d727196afdb07dc
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-06-12 17:06:58 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.11 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,15 +1,18 @@
-//피보나치 수열
+//피보나치 수열 문제
+//dp이용
 import java.util.*;
 class Solution {
     public long solution(int n) {
-        long[] memo = new long[2001];
-        memo[1] = 1; //한칸을 뛰는 방법 : 1가지
-        memo[2] = 2; //두칸을 뛰는 방법 : 2가지
-
-        for (int i = 3; i <= n; i++) {
-            memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
+        long answer = 0;
+        long[] a = new long[2001];
+        
+        a[1] = 1;
+        a[2] = 2;
+        
+        for (int i=3;i<=n;i++) {
+            a[i] = (a[i-1] + a[i-2]) % 1234567;
         }
-
-        return memo[n];
+        
+        return a[n];
     }
 }

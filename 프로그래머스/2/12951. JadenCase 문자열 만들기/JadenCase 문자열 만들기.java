ebaf1cb9349b0c57355928bc858d0f252fa65064
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-06-14 14:10:32 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,36 +1,28 @@
+//내가 푼 거
 import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        String answer = "";
-    	String[] arr = s.split(" ");
-    	
-    	for(int i=0; i<arr.length; i++) {
-    		String now = arr[i];
-    		
-    		//문자열이 공백이면 공백만 추가
-    		if(arr[i].length() == 0) {
-    			answer += " ";
-    		} 
-    		// 문자가 있다면
-    		else {
-    			
-    			answer += arr[i].substring(0, 1).toUpperCase();
-    			
-    			answer += arr[i].substring(1, now.length()).toLowerCase();
-    			// 마지막에 " " 추가
-    			answer += " ";
-    		}
-    		
-    	}
-    	
-    	// 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
-    	if(s.substring(s.length()-1, s.length()).equals(" ")){
-    		return answer;
-    	}
-    	
-    	// 맨 마지막 " " 제거하고 answer 반환
-        return answer.substring(0, answer.length()-1);
-    }
+        StringBuilder answer = new StringBuilder();
+        //맨 뒤 공백까지 추가하여 split
+        String[] ss = s.split(" ", -1); 
 
+        for (int i = 0; i < ss.length; i++) {
+            //문자열 하나가 공백이라면 공백 추가
+            if (ss[i].isEmpty()) {
+                answer.append(" ");
+            } else {
+                answer.append(ss[i].substring(0, 1).toUpperCase());
+                answer.append(ss[i].substring(1).toLowerCase());
+                answer.append(" ");
+            }
+        }
+
+        
+        if (s.substring(s.length()-1,s.length()).isEmpty()) {
+            return answer.toString();
+        }
+
+        return answer.substring(0,answer.length()-1).toString();
+    }
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 3.28 ms
+메모리: 72.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 10일 23:45:47
+2024년 06월 14일 14:10:28
 
 ### 문제 설명
 

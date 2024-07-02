f63809717a04c3634b17cca18cdb999633c81137
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-07-02 13:35:34 +0900

    [level 2] Title: 배달, Time: 3.14 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,76 +1,47 @@
+import java.util.*;
 class Solution {
-	static int cnt;
-    static int[][] arr;
-    static int[] dist;
-    static boolean[] visited;
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-
-        cnt = 0;
-        arr = new int[N + 1][N + 1];
-
-        // 인접 행렬 초기화
-        for (int i = 1; i < N + 1; i++) {
-            for (int j = 1; j < N + 1; j++) {
-                arr[i][j] = 500001;
+        int[][] map =new int[N][N];
+        
+        //모든 map값의 INF값을 넣는다.(플로이드 와샬 쓰기위해) map[정점][정점]은 0으로초기화
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (i == j){  
+                    map[i][j] = 0;
+                    continue;
+                }
+                map[i][j] = 500001;  
             }
         }
-
-        // 인접행렬 거리값 삽입
-        for (int i = 0; i < road.length; i++) {
-            if(arr[road[i][0]][road[i][1]] > road[i][2]) {
-                arr[road[i][0]][road[i][1]] = road[i][2];
-                arr[road[i][1]][road[i][0]] = road[i][2];
+        // 정점과 정점을 연결해주는 맵을 그린다. 
+        for(int[] data:road){
+            //새로운 다리가 기존에 있던 다리보다 크면 넘긴다. 작으면 갱신한다. 
+            if(map[data[0]-1][data[1]-1]<data[2]){
+                continue;
+            }
+            map[data[0]-1][data[1]-1]=data[2];
+            map[data[1]-1][data[0]-1]=data[2];
+        }
+        //플로이드 와샬 : 정점과 정점 사이의 최소거리를 구해주는 알고리즘
+        //중간 경유
+        for(int i=0; i<map.length; i++) {
+            //출발
+            for(int j=0; j<map.length; j++) {
+                //도착
+                for(int k=0; k<map.length; k++) {
+                    //i라는 중간지를 경유하는 것이 더 짧으면 갱신
+                    if(map[j][k] > map[j][i] + map[i][k])
+                        map[j][k] = map[j][i] + map[i][k];
+                }
             }
         }
-
-        // 거리, 방문 배열 만들기
-        dist = new int[N + 1];
-        // 최댓값으로 초기화
-        for (int i = 2; i < N + 1; i++) {
-            dist[i] = 500001;
+        // 1번 마을부터 출발하니, map[0]를 순환한다. 시간이 K이하면 answer++
+        for (int i = 0; i < map[0].length; i++) {
+            if (map[0][i] <= K) 
+                answer++;
         }
-        visited = new boolean[N + 1];
-        // 첫번째 마을 체크
-        visited[1] = true;
-
-        // 다익스트라 메서드 호출
-        dijkstra(N, K);
-
-        answer = cnt;
+        
         return answer;
-    }
-    // 다익스트라 메서드
-    static void dijkstra(int N, int K) {
-        // 0 ~ n-1 or 1 ~ n 까지 반복
-        for (int i = 1; i < N; i++) {
-            int min = 500001;
-            int idx = 1;
-
-            // 가장 작은 거리애 있는 인덱스값 찾기
-            for (int j = 2; j <= N; j++) {
-                if(!visited[j] && min > dist[j]) {
-                    idx = j;
-                    min = dist[j];
-                }
-            }
-
-            // 방문 체크
-            visited[idx] = true;
-
-            // 돌아가는 루트가 더 빠른지 탐색
-            for (int j = 2; j <= N ; j++) {
-                if(dist[j] > dist[idx] + arr[idx][j]) {
-                    dist[j] = dist[idx] + arr[idx][j];
-                }
-            }
-        }
-
-        // k 이하의 값 카운트
-        for (int i = 1; i <= N ; i++) {
-            if(dist[i] <= K) {
-                cnt++;
-            }
-        }
     }
 }

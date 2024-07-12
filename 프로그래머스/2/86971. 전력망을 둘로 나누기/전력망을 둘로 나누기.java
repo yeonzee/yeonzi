c491c39ea2ee6e9db4d8b68778fd2ca3811aed8f
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-07-12 22:41:27 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.86 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,7 +1,10 @@
-//2차원 배열에 wires값을 1로 넣는다
-//간선을 끊는 부분은 0으로 바꾼다
-//bfs알고리즘을 통해 간선 갯수의 차이를 구한다
-//min을 구한다
+/* 다시 풀어보려 했지만 도저히 아이디어가 생각이 안남
+어떻게 이런 생각이 드나 신기할 따름...
+2차원 배열 만들어서 해당 노드가 있다면 1
+하나씩 끊어가면서(0으로 변경) 검사
+내 노드 양쪽을 검사하는 것이므로 bfs
+개수의 차이는 n-2*cnt 하면 나옴 (이건 어케 아노)
+*/
 import java.util.*;
 class Solution {
     static int[][] arr;
@@ -9,54 +12,55 @@ class Solution {
         int answer = n;
         arr = new int[n+1][n+1];
         
-        //인접 간선은 1 
-        for(int i=0;i<wires.length;i++) {
+        //해당 노드가 있다면 1로 변경
+        for(int i=0; i<wires.length; i++) {
             arr[wires[i][0]][wires[i][1]] = 1;
             arr[wires[i][1]][wires[i][0]] = 1;
         }
         
-        //선을 하나씩 끊어보면서 순회
-        for(int i=0;i<wires.length;i++) {
+        //하나씩 끊으면서 bfs
+        for(int i=0; i<wires.length; i++) {
             int a = wires[i][0];
             int b = wires[i][1];
             
-            //선을 하나 끊고
+            //끊기
             arr[a][b] = 0;
             arr[b][a] = 0;
             
-            //bfs
-            //a를 기준으로 양 옆 간선 체크 그러므로 bfs에 a 넣음
+            //더 작은 값이 answer
+            //a 주변 노드 중심으로 bfs
             answer = Math.min(answer, bfs(n,a));
             
-            //다음 순회를 윈해 선 다시 복구
+            //다시 확인하기 위해 연결
             arr[a][b] = 1;
             arr[b][a] = 1;
         }
-        
         return answer;
     }
     
-    //bfs
     public int bfs(int n, int start) {
+        //방문확인 노드 하나만 가면 방문한 것이기 때문에 []
         boolean[] visited = new boolean[n+1];
+        //내 노드에서 출발(포함)하기 때문에 1로 시작
         int cnt = 1;
+        
+        //시작으로 방문함
+        visited[start] = true;
         
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(start);
-        //방문했다.
-        visited[start] = true;
         
-        //큐가 비었다는 말은 더이상 인접한 노드가 없다는 뜻
         while(!queue.isEmpty()) {
             //현재 노드
             int point = queue.poll();
             
-            for(int i=1;i<=n;i++) {
-                //방문하지 않았고, 현재 노드에서 간선으로 연결된 노드가 있는 경우
-                if(!visited[i] && arr[point][i]==1) {
+            //인덱스 값으로 구하기 위해 i=1
+            for(int i=1; i<=n; i++) {
+                //방문 안했고 노드끼리 연결되어 있다면
+                if(!visited[i] && arr[point][i] == 1) {
+                    cnt++;
                     visited[i] = true;
                     queue.offer(i);
-                    cnt++;
                 }
             }
         }

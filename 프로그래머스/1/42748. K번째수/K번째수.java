e0129686aaebade7d217dc62aff0ef09456e435d
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-07-15 10:15:02 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,18 +2,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int index = 0;
         
-        for (int a=0;a<commands.length;a++) {
+        for(int a=0; a<commands.length; a++) {
             int i = commands[a][0];
             int j = commands[a][1];
             int k = commands[a][2];
             
             //부분배열 만들기
-            int[] subArray = Arrays.copyOfRange(array,i-1,j);
-            Arrays.sort(subArray);
-            answer[index] = subArray[k-1];
-            index++;
+            int[] sub = Arrays.copyOfRange(array,i-1,j);
+            
+            //서브쿼리 정렬
+            Arrays.sort(sub);
+            
+            //k값 찾기
+            answer[a] = sub[k-1];
         }
         return answer;
     }

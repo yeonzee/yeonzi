319ed428b2f2cdb6905d0567584c1acb3f90abe8
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-07-15 10:58:26 +0900

    [level 2] Title: 가장 큰 수, Time: 254.07 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#qna) 
 
 ### 성능 요약
 
-메모리: 78.5 MB, 시간: 2.30 ms
+메모리: 149 MB, 시간: 254.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 26일 22:46:52
+2024년 07월 15일 10:58:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,26 +2,27 @@ import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
-        String[] arr = new String[numbers.length];
-        
-        for(int i=0;i<numbers.length;i++) {
-            arr[i] = String.valueOf(numbers[i]);
-        }
-        
-          //내림차순
-        Arrays.sort(arr,(o1,o2) -> (o2+o1).compareTo(o1+o2));
-        
-        //모든 원소가 0일 경우
-        if(arr[0].equals("0")){
-            return "0";
-        }        
-      
-        
+        String[] str = new String[numbers.length];
         StringBuilder sb = new StringBuilder();
         
-        for(int j=0;j<arr.length;j++) {
-            sb.append(arr[j]);
+        //붙이기 위해 str로 변경
+        for(int i=0; i<numbers.length; i++) {
+            str[i] = String.valueOf(numbers[i]);
         }
+        
+        //정렬
+        Arrays.sort(str, (o1,o2)-> (o2+o1).compareTo((o1+o2)));
+        
+        //정렬후엔 가장 큰 값이 앞으로 온다
+        //모든 원소가 0이면 0 반환
+        if(str[0].equals("0")) {
+            return "0";
+        }
+        
+        for(int i=0; i<str.length; i++) {
+            sb.append(str[i]);
+        }
+        
         return sb.toString();
     }
 }

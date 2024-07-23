10e2c82354286023b1d7b56b366e48af4b467f4f
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-07-23 19:02:21 +0900

    [level 2] Title: 큰 수 만들기, Time: 1711.29 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,32 +1,15 @@
-//첨부터 number의 길이 그룹을 만들어 그 그룹 내에서 큰 값 찾기
-//주의할 점: 큰 값을 찾아낸 다음의 큰 값을 골라야 한다
-import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        String answer = "";
-        
-        //만들 문자의 길이
-        int len = number.length() - k;
-        
-        //찾아낸 큰 값의 다음 값부터 구해야하기 때문에 start란 변수 만들기
-        int start = 0;
-        for(int i=0; i<len; i++) {
-            //가장 작은 값
-            char max = '0';
-            for(int j=start; j<=i+k; j++) {
-                if(number.charAt(j)>max) {
-                    max = number.charAt(j);
-                    start = j+1;
-                }
-                
-                //시간 효율성을 위해 가장 큰 수인 9면 스탑
-                if(max == '9') {
-                    break;
-                }
+        StringBuilder builder = new StringBuilder(number);
+        int length = builder.length() - k;
+
+        for (int i = 1; i < builder.length() && builder.length() > length; i++) {
+            if (builder.charAt(i) > builder.charAt(i - 1)) {
+                builder.deleteCharAt(i - 1);
+                i = Math.max(0, i - 2);
             }
-            answer += Character.toString(max);
         }
 
-        return answer;
+        return builder.substring(0, length);
     }
 }

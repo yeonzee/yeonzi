24aa0ee67879521def14a64ff76160d9d3b6f837
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-07-23 19:01:03 +0900

    [level 2] Title: 큰 수 만들기, Time: 9228.44 ms, Memory: 389 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
 
 ### 성능 요약
 
-메모리: 80 MB, 시간: 1.24 ms
+메모리: 389 MB, 시간: 9228.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 28일 01:51:47
+2024년 07월 23일 19:01:01
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,31 +1,32 @@
-//number의 길이에서 k를 뺀 값 = 문자의 길이
-//뒤에서부터 문자의 길이-1을 제외한 앞 문자중에 가장 큰 값 뽑기
-//그 값의 뒷값부터 차례로 구하기
-//탐욕법은 뒤에서부터 찾아가는 게 좋음
+//첨부터 number의 길이 그룹을 만들어 그 그룹 내에서 큰 값 찾기
+//주의할 점: 큰 값을 찾아낸 다음의 큰 값을 골라야 한다
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         String answer = "";
         
-        //만든 문자열의 길이
+        //만들 문자의 길이
         int len = number.length() - k;
         
-        //start는 가장 큰 값을 뽑은 후 다음 수부터 그 다음 큰 값을 뽑아야 하기 때문에 만든 변수
+        //찾아낸 큰 값의 다음 값부터 구해야하기 때문에 start란 변수 만들기
         int start = 0;
-        for(int i=0;i<len;i++) {
+        for(int i=0; i<len; i++) {
+            //가장 작은 값
             char max = '0';
-            for(int j=start;j<=i+k;j++) {
-                if(number.charAt(j) > max) {
+            for(int j=start; j<=i+k; j++) {
+                if(number.charAt(j)>max) {
                     max = number.charAt(j);
                     start = j+1;
                 }
-                //시간효율성을 높이기 위해 9면 break
-                if(max=='9'){
+                
+                //시간 효율성을 위해 가장 큰 수인 9면 스탑
+                if(max == '9') {
                     break;
                 }
             }
             answer += Character.toString(max);
         }
+
         return answer;
     }
 }

62ce669eb2f987941e6d47e1e6e28fd780f8c44b
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-07-29 15:43:08 +0900

    [level 3] Title: 단속카메라, Time: 26.07 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 28.91 ms
+메모리: 57 MB, 시간: 26.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 29일 15:19:47
+2024년 07월 29일 15:43:06
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,23 +1,31 @@
-import java.util.*;
+//아이디어 구상 실패..
+//1.들어오는 시점을 기준으로 내림차순 정렬
+//2.들어오는 시점에 카메라 설치, 카메라가 다음 배열에 있을 경우 continue
+//3.없으면 새롭게 들어오는 시점에 카메라 설치, answer++;
 
+import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 1;
-
-        Arrays.sort(routes,(o1,o2)->(o1[1]-o2[1]));
-        System.out.println(Arrays.deepToString(routes)); 
-
-        int max = routes[0][1];
-
-        for(int i=1;i<routes.length;i++){
-            if(routes[i][0]<=max && routes[i][1]>=max){
+        int answer = 0;
+        
+        //배열
+        Arrays.sort(routes, (o1,o2)-> o2[0]-o1[0]);
+        System.out.println(Arrays.deepToString(routes));
+        
+        int max = routes[0][0];
+        answer++;
+        
+        for(int i=1; i<routes.length; i++) {
+            if(max>=routes[i][0] && max<=routes[i][1]) {
+                //루트 사이에 카메라가 있으므로 패스
                 continue;
-            }else{
-                max = routes[i][1];
+            }
+            else {
+                max = routes[i][0];
                 answer++;
             }
         }
-
+        
         return answer;
     }
 }

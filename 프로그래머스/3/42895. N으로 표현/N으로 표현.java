0f49947c8925c8c13fab890ed246f68ecc834d36
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-06 02:05:58 +0900

    [level 3] Title: N으로 표현, Time: 36.66 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,9 +1,8 @@
 //N이 i개가 있는 통을 담을 list만들기 ex) N이 5일 때 5,55,555....
 //i가 3일때 i가1인통 (+-/*) i가 2인통 하면 됨 ex) 555, 5-55, 5*55
-//+,-같은 경우는 반대로 하면 값이 달라지므로 반대로도 해야 함 ex) 5-55, 55-5
+//-같은 경우는 반대로 하면 값이 달라지므로 반대로도 해야 함 ex) 5-55, 55-5
 //통을 만들다가 number와 같은 값이 있으면 i값 리턴
 //i가 8을 넘어가면 -1 리턴
-//개어렵다
 
 import java.util.*;
 class Solution {
@@ -22,12 +21,12 @@ class Solution {
         //i가 1일 경우는 N하나만 들어감
         list.get(1).add(N);
         
-        //number이 N과 같다면 하나만 쓰인 것이므로 1을 반환
-        if(N == number) {
-            return 1;
-        }
+        // //number이 N과 같다면 하나만 쓰인 것이므로 1을 반환
+        // if(N == number) {
+        //     return 1;
+        // }
         
-        for(int i=2; i<9; i++) {
+        for(int i=1; i<9; i++) {
             
             //5,55,555와 같은 숫자 넣기
             StringBuilder sb = new StringBuilder();

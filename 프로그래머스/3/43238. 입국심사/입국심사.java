c590f41a5de374c71975e886cc6fa862e4267053
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-20 11:29:48 +0900

    [level 3] Title: 입국심사, Time: 134.31 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,34 +1,30 @@
-//이분탐색인걸 눈치챌 수 있을련지...
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         //정렬
         Arrays.sort(times);
+        
         long answer = 0;
-        long left = 0; //가장 작은 값
-        long right = times[times.length-1] * (long)n; //가장 큰 값 ex)10*6
+        
+        long left = 0;
+        long right = times[times.length-1] * (long)n;  //가장 큰 값
         
         while(left <= right) {
             long mid = (left+right) / 2;
-            //입국심사 할 수 있는 사람 수
-            long sum = 0;
+            long sum = 0;  //입국심사 할 수 있는 사람 수
             
             for(int i=0; i<times.length; i++) {
                 sum += mid / times[i];
             }
             
-            //n보다 작을 경우 시간이 더 필요함
-            if(sum < n) {
+            if(sum < n) {  //시간이 더 필요하다
                 left = mid+1;
             }
-            //n보다 크거나 같을 경우 더 최적의 값이 있을 수 있음
             else {
                 right = mid-1;
                 answer = mid;
             }
         }
-        
-        
         return answer;
     }
 }

02d17962dc16eba2cb268f211f49f2a2ba4ae1b5
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-20 22:07:14 +0900

    [Bronze V] Title: Hello World, Time: 88 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void solution() {
+        System.out.print("Hello World!");
+    }
+    
+    public static void main(String[] args) {
+        solution();
+    }
+}

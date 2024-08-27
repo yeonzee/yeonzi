e50ebea24bda7f5862d167fa8e3c74754500a118
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-27 14:15:48 +0900

    [level 2] Title: 타겟 넘버, Time: 7.34 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,25 +1,27 @@
+//dfs
 import java.util.*;
 class Solution {
     static int answer = 0;
     public int solution(int[] numbers, int target) {
-        
         dfs(numbers, target, 0, 0);
         return answer;
     }
     
     public void dfs(int[] numbers, int target, int sum, int depth) {
-        //종료조건
+        //종료 조건
         if(depth == numbers.length) {
             if(sum == target) {
                 answer++;
             }
             return;
         }
-        
+         
+            
         //더하기
         dfs(numbers, target, sum+numbers[depth], depth+1);
-        
+
         //빼기
         dfs(numbers, target, sum-numbers[depth], depth+1);
+        
     }
 }

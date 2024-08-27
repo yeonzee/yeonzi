19528e3bf266603c9e0f8c82eaf1478741fe6507
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-27 22:56:41 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,43 +1,41 @@
-//최소단계여서 bfs로 구하려다가 실패
-//dfs 풀이
+//dfs
 import java.util.*;
 class Solution {
     static boolean[] visited;
-    static int answer = 0;
+    static int answer;
     public int solution(String begin, String target, String[] words) {
+        answer = 0;
         visited = new boolean[words.length];
-        
         dfs(begin, target, words, 0);
-        
         return answer;
     }
     
-    public void dfs(String begin, String target, String[] words, int count) {
+    public void dfs(String word, String target, String[] words, int depth) {
         //종료조건
-        if(begin.equals(target)) {
-            answer = count;
+        if(word.equals(target)) {
+            answer = depth;
             return;
         }
         
         for(int i=0; i<words.length; i++) {
-            //방문한 경우 무시
+            
+            //이미 방문했다면 무시
             if(visited[i]) {
                 continue;
             }
             
-            //다른 글자 수 세기
-            int k = 0;
-            for(int j=0; j<words[i].length(); j++) {
-                if(begin.charAt(j) != words[i].charAt(j)) {
-                    k++;
+            String w = words[i];
+            
+            int count = 0;
+            for(int j=0; j<w.length(); j++) {
+                if(word.charAt(j) != w.charAt(j)) {  //같지 않은게 1개만 있어야 함
+                    count++;
                 }
             }
             
-            //한글자만 다른 경우 단어 바꾸기
-            if(k==1) {
+            if(count==1) {
                 visited[i] = true;
-                dfs(words[i], target, words, count+1);
-                //백트래킹
+                dfs(w, target, words, depth+1);
                 visited[i] = false;
             }
         }

86e197da000e941fc042325f88de6e47282d8335
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-27 15:43:56 +0900

    [level 2] Title: 조이스틱, Time: 0.09 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,5 +1,3 @@
-//n을 중심으로 앞에 있으면 up, 뒤에 있으면 down
-//A를 만나면 역주행
 import java.util.*;
 class Solution {
     public int solution(String name) {
@@ -7,26 +5,25 @@ class Solution {
         int move = name.length();
         
         for(int i=0; i<name.length(); i++) {
-            char c = name.charAt(i);
+            char now = name.charAt(i);
             
             //더 작은 값 넣어주기
-            answer += Math.min(c-'A','Z'-c+1);
+            answer += Math.min(now-'A', 'Z'-now+1);
             
-            //역주행 조건
+            //역주행
             int index = i+1;
             //A가 여러개일 수도 있기 때문에
             while(index<name.length() && name.charAt(index) == 'A') {
                 index++;
             }
             
-            //정주행하는 것과 A를 기점으로 돌아가는 것 중 더 최솟값 고르기
-            move = Math.min(move,i*2 + name.length()-index);
+            //직직하는 것과 A가 끝난 시점에서 역주행하는 것 중 짧은 것 고르기 ex)BBBAAAAC
+            move = Math.min(move, i*2 + name.length()-index);
             
-            //이건 뭐지
-            //애초에 뒤에서 시작하는 것이 더 빠른 경우도 구해주기 BBBBAABB
-            move = Math.min(move,(name.length()-index)*2 + i);
-            
+            //이건 잘 모르겠음
+            //애초에 뒤에서 시작하는 것이 더 빠른 경우도 구해주기
+            move = Math.min(move, (name.length()-index)*2 + i);
         }
-        return answer+move; 
+        return answer+move;
     }
 }

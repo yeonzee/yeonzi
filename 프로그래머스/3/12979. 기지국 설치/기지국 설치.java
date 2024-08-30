ddcf7526cae87d88b40b66004024f0de4db9da2d
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-31 02:38:04 +0900

    [level 3] Title: 기지국 설치, Time: 1.04 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,44 +1,19 @@
-//for문 여러개 돌리려고 했는데 n이 2억이다 포기
-//1.구간별로 나눈다
-//2.w*2+1 -> 전파 전달 범위
-import java.util.*;
+//더 짧은 방법
 class Solution {
     public int solution(int n, int[] stations, int w) {
+        int position = 1;
+        int si = 0;
         int answer = 0;
-        int leftStart = 1;
-        
-        Arrays.sort(stations);
-
-        for (int sub : stations) {
-            if (leftStart < sub - w) {
-                int leftEnd = sub - w;
-
-                int length = leftEnd - leftStart;
-
-                int count = length / (w * 2 + 1);
-                if (length % (w * 2 + 1) != 0)  //딱 나누어 떨어지지 않으면 추가 기지국이 필요하다는 뜻
-                    count++;
-
-                answer += count;
+        while(position <= n) {
+            if(si < stations.length && stations[si] - w <= position) {
+                position = stations[si] + w + 1;
+                si++;
             }
-
-            leftStart = sub + w + 1;
+            else {
+                answer++;
+                position += 2*w+1;
+            }
         }
-
-        if(stations[stations.length-1] + w + 1 <= n){
-            leftStart = stations[stations.length-1] + w + 1;
-
-            int leftEnd = n + 1;
-
-            int length = leftEnd - leftStart;
-
-            int count = length / (w * 2 + 1);
-            if (length % (w * 2 + 1) != 0)
-                count++;
-
-            answer += count;
-        }
-
         return answer;
     }
 }

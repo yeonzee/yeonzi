f1c6d192ebf73a1b553304e22ec65076f30049cb
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-30 20:23:35 +0900

    [level 3] Title: 단속카메라, Time: 11.14 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,31 +1,27 @@
-//아이디어 구상 실패..
-//1.들어오는 시점을 기준으로 내림차순 정렬
-//2.들어오는 시점에 카메라 설치, 카메라가 다음 배열에 있을 경우 continue
-//3.없으면 새롭게 들어오는 시점에 카메라 설치, answer++;
-
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
         
-        //배열
-        Arrays.sort(routes, (o1,o2)-> o2[0]-o1[0]);
-        System.out.println(Arrays.deepToString(routes));
+        //나가는 지점을 기준으로 오름차순
+        Arrays.sort(routes, (o1,o2) -> o1[1]-o2[1]);
+        // for(int[] a:routes) {
+        //     System.out.println(Arrays.toString(a));
+        // }
         
-        int max = routes[0][0];
+        //나가는 지점을 기준으로 카메라 설치
+        int max = routes[0][1];
         answer++;
         
         for(int i=1; i<routes.length; i++) {
-            if(max>=routes[i][0] && max<=routes[i][1]) {
-                //루트 사이에 카메라가 있으므로 패스
+            if(max>=routes[i][0] && max<=routes[i][1]) {  //이미 카메라가 있다는 것
                 continue;
             }
             else {
-                max = routes[i][0];
+                max = routes[i][1];
                 answer++;
             }
         }
-        
         return answer;
     }
 }

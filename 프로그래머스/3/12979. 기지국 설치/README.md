ce0708f5c6250023888d769d169f85fa28e5db3c
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-08-31 03:02:26 +0900

    [level 3] Title: 기지국 설치, Time: 1.03 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979#qna) 
 
 ### 성능 요약
 
-메모리: 53.7 MB, 시간: 1.04 ms
+메모리: 52.6 MB, 시간: 1.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 31일 02:38:01
+2024년 08월 31일 03:02:24
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,19 +1,25 @@
-//더 짧은 방법
+//이게 더 좋은 방법인듯
+//첫번째 아파트부터 조사하여 하나씩 늘려간다 (while을 통해)
+//w*2+1 -> 전파 범위
+import java.util.*;
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        int position = 1;
-        int si = 0;
         int answer = 0;
-        while(position <= n) {
-            if(si < stations.length && stations[si] - w <= position) {
-                position = stations[si] + w + 1;
-                si++;
+        int st = 0;
+        int apt = 1;  //첫번째 아파트부터 조사
+        
+        while (apt <= n) {
+            //현재 기지국으로 처리 가능한지 먼저!!! 확인함
+            if(st<stations.length && stations[st]-w <= apt) {  
+                apt = stations[st] + w + 1;  //처리 가능하다면 처리한 다음 아파트로 갱신
+                st++;
             }
-            else {
+            else {  //추가 기지국 필요
                 answer++;
-                position += 2*w+1;
+                apt += 2*w+1;
             }
         }
+
         return answer;
     }
 }

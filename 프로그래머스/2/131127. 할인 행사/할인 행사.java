dd9e7c4d0f79ae1b66c517ba179e8613d234646f
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-02 16:37:58 +0900

    [level 2] Title: 할인 행사, Time: 75.97 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,34 +3,41 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        // 초기 맵 상태 저장
-        for (int i = 0; i < want.length; i++) {
-            map.put(want[i], number[i]);
+        int total = 0;
+
+        // 구매해야 하는 전체 아이템 수 계산
+        for(int n : number) {
+            total += n;
         }
-        
-        // 10일 단위로 검사
-        for (int i = 0; i <= discount.length - 10; i++) {
-            HashMap<String, Integer> currentMap = new HashMap<>(map); // 현재 상태 복사
-            for (int j = i; j < i + 10; j++) {
-                if (currentMap.containsKey(discount[j])) {
-                    currentMap.put(discount[j], currentMap.get(discount[j]) - 1);
+
+        // 슬라이딩 윈도우 방식 적용
+        for (int i = 0; i <= discount.length - total; i++) {
+            // number 배열 복사 (초기화 용도)
+            int[] temp = Arrays.copyOf(number, number.length);
+
+            // 현재 윈도우의 아이템 체크
+            for (int j = i; j < i + total; j++) {
+                for (int k = 0; k < want.length; k++) {
+                    if (discount[j].equals(want[k])) {
+                        temp[k]--;  // 해당 품목 수량 감소
+                    }
                 }
             }
-            // 현재 상태가 모두 0인지 검사
+
+            // 모든 수량이 0이 되었는지 확인
             boolean allZero = true;
-            for (int k = 0; k < want.length; k++) {
-                if (currentMap.get(want[k]) > 0) {
+            for (int n : temp) {
+                if (n != 0) {
                     allZero = false;
                     break;
                 }
             }
+
             if (allZero) {
                 answer++;
             }
         }
-        
+
         return answer;
     }
 }

16aa48f691de64f3bbbc78e04a513520f97e305a
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-02 15:19:47 +0900

    [level 2] Title: 소수 찾기, Time: 31.10 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,52 +1,43 @@
-//모든 조합 만들기(중복을 피하기 위해 set사용)
-//소수인지 확인
 import java.util.*;
 class Solution {
     static HashSet<Integer> set = new HashSet<>();
-    
     public int solution(String numbers) {
+        
         int answer = 0;
         
-        recursive("",numbers);
+        //중복없이 모든 조합 set에 저장
+        recursive("", numbers);
         
         for(int num:set) {
-            if(isboolean(num)) {
-                //소수임
+            if(isboolean(num)) {  //소수라면
                 answer++;
             }
         }
-        
-        
         return answer;
     }
     
-    //모든 조합 중복없이 set에 저장
     public void recursive(String s, String others) {
         if(!s.equals("")) {
             set.add(Integer.valueOf(s));
         }
         
-        //재귀
         for(int i=0; i<others.length(); i++) {
-            recursive(s+others.charAt(i),others.substring(0,i)+others.substring(i+1));
+            //재귀
+            recursive(s+others.charAt(i), others.substring(0,i)+others.substring(i+1));
         }
-        
     }
     
-    //소수인지 확인
     public boolean isboolean(int num) {
-        if (num==0 || num == 1) {
+        if(num==0 || num==1) {
             return false;
         }
-        else {
-            for(int i=2; i<=(int)Math.sqrt(num); i++) {
-                //소수아님
-                if(num % i == 0) {
+        else{
+            for(int i=2; i<=Math.sqrt(num); i++) {
+                if(num % i == 0) { //소수아님
                     return false;
                 }
             }
         }
         return true;
     }
-    
 }

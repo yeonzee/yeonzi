7ad8f3f8d13e4aac5b4d0411ec09102bcb7f430b
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-04 01:50:11 +0900

    [level 2] Title: N개의 최소공배수, Time: 1.20 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,37 +1,23 @@
-/* 
-우선 최소공배수는 (n*m / 최대공약수)로 구할 수 있음
-최대공약수는 gcd
-*/
 import java.util.*;
 class Solution {
     public int solution(int[] arr) {
-        int answer = 0;
-        //arr에 원소가 1개인 경우
-        if (arr.length == 1) {
-            answer = arr[0];
+        Arrays.sort(arr);
+        
+        int answer = arr[0]; // 첫 번째 요소로 초기화
+        
+        for (int i = 1; i < arr.length; i++) {
+            int num = gcd(answer, arr[i]); // 현재까지의 최소 공배수와 다음 숫자의 최대 공약수
+            answer = (answer * arr[i]) / num; // 최소 공배수 계산
         }
         
-        int g = gcd(arr[0],arr[1]); // 처음 두 원소의 최대공약수
-        answer = (arr[0] * arr[1]) / g; // 처음 두 원소의 최소공배수
-        
-        //arr의 길이가 3이상인 경우 앞 두 원소의 최소공배수와 그 다음에 오는 원소의 최소공배수 
-        
-        if (arr.length > 2) {
-            for (int i=2; i<arr.length;i++) {
-                g = gcd(answer, arr[i]);
-                answer = (answer * arr[i]) / g;
-            }
-        }
         return answer;
     }
-    //최대공약수
-    public static int gcd(int a, int b) {
-        int r = a%b;
-        if (r==0) {
-            return b;
-        }
-        else {
-            return gcd(b,r);
+    
+    public int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b); 
         }
     }
 }

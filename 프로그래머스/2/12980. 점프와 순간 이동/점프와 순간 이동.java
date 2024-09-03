26bca156069b0ecbaf2999136a384963ab518bee
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-03 22:57:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,15 +1,20 @@
+//top-down 형식으로 풀이
 import java.util.*;
 
 public class Solution {
     public int solution(int n) {
         int ans = 0;
-        while(n > 0) {
-            if (n % 2 == 0) {
-                n /= 2; 
+        
+        while(true) {
+            if(n == 0) {
+                break;
             }
-            else {
-                n -= 1;
-                //점프횟수
+            
+            if(n % 2 == 0) {  //짝수인 경우는 순간이동
+                n /= 2;
+            }
+            else {  //홀수인 경우는 점프
+                n--;
                 ans++;
             }
         }

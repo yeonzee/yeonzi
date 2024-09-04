0c1084cdfcf091d2282c881386e1850134948f86
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-04 11:36:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 0.11 ms
+메모리: 73.1 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 12일 17:06:56
+2024년 09월 04일 11:36:46
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,18 +1,18 @@
-//피보나치 수열 문제
-//dp이용
+//피보나치
 import java.util.*;
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        long[] a = new long[2001];
+        int[] arr = new int[2001];
         
-        a[1] = 1;
-        a[2] = 2;
+        arr[1] = 1;
+        arr[2] = 2;
         
-        for (int i=3;i<=n;i++) {
-            a[i] = (a[i-1] + a[i-2]) % 1234567;
+        for(int i=3; i<=n; i++) {
+            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
         }
         
-        return a[n];
+        
+        return arr[n];
     }
 }

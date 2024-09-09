8452d8dfad287f9cd25320f5e6cac741c030e41d
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-10 02:17:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
 
 ### 성능 요약
 
-메모리: 77.6 MB, 시간: 0.03 ms
+메모리: 76.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 12일 16:12:27
+2024년 09월 10일 02:17:52
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,18 +1,21 @@
+//푸는 방법 까먹음
 import java.util.*;
 class Solution {
     public int solution(int n, int a, int b) {
         int answer = 0;
         
         while(true) {
-            a = (a+1) / 2;
+            a = (a+1) / 2;  //a가 4일 경우 (4+1)/2 = 2이다 = 2번 경기라는 뜻 (a가 3이여도 +1을 하기 때문에 2번 경기가 됨)
             b = (b+1) / 2;
             
             answer++;
             
-            if (a==b) {
+            //a랑 b가 같아지면 같은 경기에서 만났다는 뜻
+            if(a==b) {
                 break;
             }
         }
+
         return answer;
     }
 }

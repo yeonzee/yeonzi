3125145ac4ef0a167c68fb04b70d78f0e74c6c49
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-11 00:12:21 +0900

    [level 2] Title: 교점에 별 만들기, Time: 26.30 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/README.md
+++ 프로그래머스/2/87377. 교점에 별 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 교점에 별 만들기 - 87377 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87377#qna) 
 
 ### 성능 요약
 
-메모리: 89.8 MB, 시간: 26.94 ms
+메모리: 86.4 MB, 시간: 26.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 10일 22:16:50
+2024년 09월 11일 00:12:18
 
 ### 문제 설명
 
--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,61 +1,75 @@
-//답지
+//너무 어렵다
+//참고 사항의 식을 이용하여 존재하는 교점을 넣을 리스트 생성
+//최대x, 최소x, 최대y, 최소y를 구하여 범위 설정
+//.으로 채운 배열 생성
+//for문을 통해 리스트에 있는 교점들을 *로 변환
+//long으로 변경해야 완전 통과됨
 import java.util.*;
 class Solution {
-    static long minx = Long.MAX_VALUE, miny = Long.MAX_VALUE;
-    static long maxx = Long.MIN_VALUE, maxy = Long.MIN_VALUE;
-
     public String[] solution(int[][] line) {
-        String[] answer = {};
-        List<long[]> points = new ArrayList<>();
-
-        // x = (bf - ed) / (ad - bc)
-        // y = (ec - af) / (ad - bc)
-        long x, y;
-        for (int i = 0; i < line.length - 1; i++) {
+        
+        ArrayList<long[]> arr = new ArrayList<>();
+        
+        //최대값들 구하기
+        long minx = Long.MAX_VALUE;
+        long miny = Long.MAX_VALUE;
+        long maxx = Long.MIN_VALUE;
+        long maxy = Long.MIN_VALUE;
+        
+        for(int i=0; i<line.length; i++) {
             long a = line[i][0];
             long b = line[i][1];
             long e = line[i][2];
-            for (int j = i + 1; j < line.length; j++) {
+            for(int j=i; j<line.length; j++) {
                 long c = line[j][0];
                 long d = line[j][1];
                 long f = line[j][2];
-
-                long adbc = a * d - b * c;
-                if (adbc == 0) continue; // 직선이 평행할 경우
-
-                long bfed = b * f - e * d;
-                if (bfed % adbc != 0) continue; // x가 정수가 아닐 경우
-
-                long ecaf = e * c - a * f;
-                if (ecaf % adbc != 0) continue; // y가 정수가 아닐 경우
-
-                x = bfed / adbc;
-                y = ecaf / adbc;
-                points.add(new long[] { x, y }); // 좌표를 배열로 저장
-
+                
+                if((a*d)-(b*c) == 0) {  //평행하거나 같은 직선이므로 무시
+                    continue;
+                } 
+                if(((b*f)-(e*d)) % ((a*d)-(b*c)) != 0) {  //x가 정수가 아님
+                    continue;
+                }
+                if(((e*c)-(a*f)) % ((a*d)-(b*c)) != 0) {  //y가 정수가 아님
+                     continue;
+                }
+                
+                long x = ((b*f)-(e*d)) / ((a*d)-(b*c));
+                long y = ((e*c)-(a*f)) / ((a*d)-(b*c));
+                
+                arr.add(new long[]{x,y});
+                
                 minx = Math.min(minx, x);
                 miny = Math.min(miny, y);
                 maxx = Math.max(maxx, x);
                 maxy = Math.max(maxy, y);
+                
             }
         }
-
-        // 좌표계 크기 계산
-        long height = maxy - miny + 1;
-        long width = maxx - minx + 1;
-        answer = new String[(int) height];
+        
+        //높이 넓이 구하기 (ex)2,5일때 2,3,4,5가 포함되어야 하므로 +1을 해줌
+        long height = maxy - miny+1;
+        long width = maxx - minx+1;
+        
+        String[] answer = new String[(int)height];
+        
+        //가로 .으로 채우기
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < width; i++) {
+        for(int i=0; i<width; i++) {
             sb.append(".");
         }
+        
+        //answer값에 sb 채우기
         Arrays.fill(answer, sb.toString());
-
-        // 별표를 바로 배열에 찍기
-        for (long[] p : points) {
+        
+        //교점 별표로 변경하기
+        //이해실패
+        for(long[] p:arr) {
             answer[(int) (maxy - p[1])] = answer[(int) (maxy - p[1])].substring(0, (int) (p[0] - minx)) + "*"
                     + answer[(int) (maxy - p[1])].substring((int) (p[0] - minx) + 1);
         }
-
+        
         return answer;
     }
 }

d7075475ccf9666f43da10130c8c6029aa4e4f29
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-11 18:03:19 +0900

    [level 2] Title: 귤 고르기, Time: 86.51 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,24 +2,29 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
+        
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        for (int num : tangerine) {
-            map.put(num,map.getOrDefault(num,0)+1);
+        for(int num:tangerine) {
+            map.put(num, map.getOrDefault(num,0)+1);
         }
         
-        List<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, (v1, v2) -> (map.get(v2).compareTo(map.get(v1))));
+        //value기준 내림차순 정렬
+        //map은 순서를 보장하지 않는 자료구조이다
+        //list로 변환하여 정렬해야 함
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (o1,o2) -> map.get(o2)-map.get(o1));
         
-        int sum=0;
-        for (int a : list) {
-           sum += map.get(a);
+        int sum = 0;
+        for(int i=0; i<list.size(); i++) {
+            sum += map.get(list.get(i));
             answer++;
-            if (sum>=k) {
+            
+            if(sum >= k) {
                 break;
             }
         }
-
+        
         return answer;
     }
 }

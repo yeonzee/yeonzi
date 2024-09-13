99c5fc2716984d67c5d6da86be9fe0033c1fac15
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-13 21:59:24 +0900

    [level 1] Title: 체육복, Time: 0.57 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,48 +1,39 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        int count = 0;
-        
-        //정렬
+        int answer = n;
         Arrays.sort(lost);
         Arrays.sort(reserve);
         
-        //빌려준 경우를 체크하기 위한 boolean
-        boolean[] lost_check = new boolean[lost.length];
-        boolean[] reserve_check = new boolean[reserve.length];
+        boolean[] l_check = new boolean[lost.length];
+        boolean[] r_check = new boolean[reserve.length];
         
-        //여벌이 있는데 도난 당한 경우
-        for(int i=0; i< lost.length; i++) {
-            for(int j=0; j<reserve.length; j++) {
-                if(!lost_check[i] && !reserve_check[j] && lost[i]==reserve[j]) {
-                    lost_check[i] = true;
-                    reserve_check[j] = true;
-                    
+        //내가 내것을 잃어버리는 경우 먼저 구하기
+        for(int i=0; i<reserve.length; i++) {
+            for(int j=0; j<lost.length; j++) {
+                if(!r_check[i] && !l_check[j] && reserve[i] == lost[j]) {  //내가 내것을 잃어버림
+                    l_check[j] = true;
+                    r_check[i] = true;
                 }
             }
         }
         
-        //나머지 계산
-        for(int i=0; i<lost.length; i++) {
-            for(int j=0; j<reserve.length; j++) {
-                if(!lost_check[i] && !reserve_check[j] && (lost[i]==reserve[j]-1 || lost[i]==reserve[j]+1)) {
-                    lost_check[i] = true;
-                    reserve_check[j] = true;
-                    
+        //내 체육복을 잃어버린 경우를 제외하고 빌려주는 경우
+        for(int i=0; i<reserve.length; i++) {
+            for(int j=0; j<lost.length; j++) {
+                
+                if(!r_check[i] && !l_check[j] && (reserve[i]-1 == lost[j] || reserve[i]+1 == lost[j])) {  //앞뒤 친구한테 빌려줌
+                    l_check[j] = true;
+                    r_check[i] = true;
                 }
             }
         }
         
-        //빌려준 갯수
-        for(int i=0; i<lost_check.length; i++) {
-            if(lost_check[i]) {
-                count++;
+        for(int i=0; i<l_check.length; i++) {
+            if(!l_check[i]) {
+                answer--;
             }
         }
-        
-        //전체 학생 수 - 빌려야 하는 학생 수 + 빌린 학생 수
-        answer = n-lost_check.length+count;
         return answer;
     }
 }

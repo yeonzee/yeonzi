936d7c50e7ba18445b0eac82e6974f9cef46e8a5
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-13 16:57:03 +0900

    [level 2] Title: [1차] 캐시, Time: 49.34 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680#qna) 
 
 ### 성능 요약
 
-메모리: 81.6 MB, 시간: 2.05 ms
+메모리: 112 MB, 시간: 49.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 17일 13:58:04
+2024년 09월 13일 16:57:00
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,47 +1,39 @@
-/*
-LRU는 가장 최근에 것만 사용
-새로운 값이 들어올 경우 가장 오래된 값 삭제 후 삽입
-캐시 안에 해당 값이 있는 경우 +1
-캐시 안에 해당 값이 없는 경우 +5
-ArrayList 사용
-*/
+//캐시크기만큼의 배열 생성
+//새로 들어가면 +5
+//이미 있으면 +1
+//넣기 시작한 시점부터 arr에 도시이름이 있는지 확인해야 함
 import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> arr = new ArrayList<>();
         
-        //캐시크기가 0인 경우
+        //캐시크기가 0일 경우
         if(cacheSize == 0) {
-            return cities.length * 5;
+            return cities.length*5;
         }
         
-        for (int i=0;i<cities.length;i++) {
-            //캐시에 이미 해당 값이 있다면
-            String str = cities[i].toLowerCase(); //대소문자 구분x
-            if (list.contains(str)) {
-                //해당 도시 지우고 최근 값으로 갱신
-                list.remove(str);
-                list.add(str);
-                answer += 1;
-            }
-            //캐시에 해당 값이 없다면
-            else {
-                //꽉 찼다면
-                if (list.size() == cacheSize) {
-                    //가장 오래된 값 지우기
-                    list.remove(0);
-                    list.add(str);
-                    answer += 5;
-                }
-                //비었다면
-                else {
-                    list.add(str);
-                    answer += 5;
-                }
-                
+        for(int i=0; i<cities.length; i++) {
+            String str = cities[i].toLowerCase();  //다 소문자로 변환
+            
+            if(arr.contains(str)) {  //arr에 있으면 +1
+                //LRU이기 때문에 해당하는 도시 지우고 최근 도시 값으로 갱신
+                arr.remove(str);
+                arr.add(str);
+                answer++;
             }
             
+            else {  //arr에 없으면 +5
+                if(arr.size() < cacheSize) {  //캐시크기보다 작다면 삭제없이 삽입
+                    arr.add(str);
+                    answer += 5;
+                }
+                else {  //캐시크기보다 크다면 삭제 후 삽입
+                    arr.remove(0);
+                    arr.add(str);
+                    answer += 5;
+                }
+            }
         }
         return answer;
     }

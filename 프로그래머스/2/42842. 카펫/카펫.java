fa064506047bbef3a649d3a497b8dbcdc32db03f
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-20 17:43:30 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,20 @@
-
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        for(int i=1; i<=brown; i++) {
-            for(int j=1; j<=yellow; j++) {
-                if(i*j == yellow && (i*2 + j*2)+4 == brown) {
-                    answer[0] = i+2;
-                    answer[1] = j+2;
-                    break;
+        
+        for(int i=1; i<=yellow; i++) {
+            if(yellow % i == 0) {  //딱 나누어 떨어질때만(정수만 고려)
+                int j = yellow / i;
+                
+                if(i*2 + j*2 + 4 == brown) {
+                answer[0] = Math.max(i+2,j+2);
+                answer[1] = Math.min(i+2,j+2);
+                break;
                 }
             }
+            
+            
         }
         return answer;
     }

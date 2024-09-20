a5c7438efe0e111d7b003a4fc824ec215201d850
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-20 18:37:15 +0900

    [level 2] Title: 피로도, Time: 3.53 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,31 +1,30 @@
 //dfs
 import java.util.*;
 class Solution {
-    static int answer;
     static boolean[] visited;
+    static int answer = 0;
     public int solution(int k, int[][] dungeons) {
-        answer = 0;
         visited = new boolean[dungeons.length];
         
-        dfs(0,k,dungeons);
+        dfs(0, dungeons, k);
         
         return answer;
     }
     
-    public void dfs(int depth, int k, int[][] dungeons) {
-        if(dungeons.length == answer) {
-            return;
-        }
+    public void dfs(int depth, int[][] dungeons, int k) {
         
-        for(int i=0; i<dungeons.length; i++) {
-            //k보다 작고 방문하지 않았다면 탐방 가능
-            if(k>=dungeons[i][0] && !visited[i]) {
-                visited[i] = true;
-                //다시 탐험
-                dfs(depth+1, k-dungeons[i][1], dungeons);
-                visited[i] = false;
+        for(int j=0; j<dungeons.length; j++) {
+            if(!visited[j] && k>=dungeons[j][0]) {
+                visited[j] = true;
+                k -= dungeons[j][1];
+                dfs(depth+1, dungeons, k);
+                
+                //백트래킹
+                visited[j] = false;
+                k += dungeons[j][1];
             }
         }
-        answer = Math.max(answer,depth);
+        
+        answer = Math.max(answer, depth);
     }
 }

4716ba74ee547310a0de2acf460cf4135c051a1e
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-20 23:55:12 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 10.14 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -6,20 +6,20 @@ class Solution {
         int answer = n;
         map = new int[n+1][n+1];
         
-        //연결되어 있는 것들은 1로 초기화
+        //연결되어 있는 부분 1로 초기화
         for(int i=0; i<wires.length; i++) {
             map[wires[i][0]][wires[i][1]] = 1;
             map[wires[i][1]][wires[i][0]] = 1;
         }
         
+        //하나씩 끊어보기
         for(int i=0; i<wires.length; i++) {
-            //끊어보기
             map[wires[i][0]][wires[i][1]] = 0;
             map[wires[i][1]][wires[i][0]] = 0;
             
-            answer = Math.min(answer,bfs(n, wires[i][0]));
+            answer = Math.min(answer,bfs(n, wires[i][0]));  //wires[i][0] 만 넣는 이유: 자른 구간을 기준으로 한 구역만 계산
             
-            //다른 노드를 알아보기 위해 다시 연결
+            //백트래킹
             map[wires[i][0]][wires[i][1]] = 1;
             map[wires[i][1]][wires[i][0]] = 1;
         }
@@ -27,30 +27,32 @@ class Solution {
         return answer;
     }
     
+    //bfs에서 각 전력망들의 갯수 차이 구하기
     public int bfs(int n, int start) {
-        //방문 확인
+        //방문처리
         boolean[] visited = new boolean[n+1];
         
-        //일단 나 자신은 방문하고 시작하므로 1
+        //시작하면서 나를 방문하기 때문에 1로 초기화
         int cnt = 1;
         
-        //나 자신 방문 처리
+        //나 자신 방문처리
         visited[start] = true;
         
         Queue<Integer> q = new LinkedList<>();
         q.offer(start);
         
         while(!q.isEmpty()) {
-            int point = q.poll();
+            int temp = q.poll();
             
-            for(int i=1; i<=n; i++) {
-                if(!visited[i] && map[point][i] == 1) {  //방문하지 않았고 연결되어 있다면
-                    cnt++;
+            for(int i=1; i<n+1; i++) {
+                if(!visited[i] && map[temp][i] == 1) {  //갈 수 있음
                     visited[i] = true;
-                    q.add(i);
+                    cnt++;
+                    q.offer(i);
                 }
             }
         }
+        
         return Math.abs(n-2*cnt);
     }
 }

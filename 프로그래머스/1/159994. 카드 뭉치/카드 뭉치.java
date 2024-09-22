d2cca154a0107a478e67d2a7ffeb56e9b7bd89fc
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-22 22:23:44 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,20 +1,23 @@
-//cards1, cards2의 첫번째 값이랑 goal의 값들을 비교하여 해당하는 값이 있으면 원배열에서 제거
 import java.util.*;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "";
-        ArrayList<String> c1 = new ArrayList<>(Arrays.asList(cards1));
-        ArrayList<String> c2 = new ArrayList<>(Arrays.asList(cards2)); 
         
-        for (int i = 0; i < goal.length; i++) {
-            if (!c1.isEmpty() && c1.get(0).equals(goal[i])) {
-                c1.remove(0);
-            } else if (!c2.isEmpty() && c2.get(0).equals(goal[i])) {
-                c2.remove(0);
-            } else {
+        int idx1 = 0;
+        int idx2 = 0;
+        
+        for(int i=0; i<goal.length; i++) {
+            if(idx1<cards1.length && goal[i].equals(cards1[idx1])) {
+                idx1++;
+            }
+            else if(idx2<cards2.length && goal[i].equals(cards2[idx2])) {
+                idx2++;
+            }
+            else {
                 return "No";
             }
         }
+         
         return "Yes";
     }
 }

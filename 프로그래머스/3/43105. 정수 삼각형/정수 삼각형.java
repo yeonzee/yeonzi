d6bf68def3c738775ac3998cb57e2498c5524ee8
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-23 19:26:42 +0900

    [level 3] Title: 정수 삼각형, Time: 8.89 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,33 +1,39 @@
-//삼각형 왼쪽으로 밀기
-//합에 대한 배열 만들기
+//왼쪽으로 밀어서 누적값 저장
+//누적값은 위에 있는 값 중 큰 값으로 가져오기
+
 import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
         
-        int[][] arr = new int[triangle.length][triangle.length];
+        int[][] map = new int[triangle.length][triangle.length];
         
-        arr[0][0] = triangle[0][0];
+        map[0][0] = triangle[0][0];
         
         for(int i=1; i<triangle.length; i++) {
-            for(int j=0; j<triangle[i].length; j++) {
-                //첫번째 값은 위에 값만 더해야 함
+            for(int j=0; j<=i; j++) {
+                //첫번째 열은 바로 위의 값만 받을 수 있다
                 if(j==0) {
-                    arr[i][j] = arr[i-1][j] + triangle[i][j];
+                    map[i][j] = map[i-1][j] + triangle[i][j];
                 }
-                //마지막 값은 윗 대각선의 값만 더해야 함
-                else if(j==triangle[i].length-1) {
-                    arr[i][j] = arr[i-1][j-1] + triangle[i][j];
+                
+                //마지막 열은 대각선 위의 값만 받을 수 있다
+                else if(j==i) {
+                    map[i][j] = map[i-1][j-1] + triangle[i][j];
                 }
-                else { //두개 다 아니라면 위에 있는 값 중 큰 값을 더해야 함
-                    arr[i][j] = Math.max(arr[i-1][j], arr[i-1][j-1]) + triangle[i][j];
-                }   
-            } 
+                
+                //중간 값들은 더 큰 값을 받을 수 있다
+                else {
+                    map[i][j] = Math.max(map[i-1][j], map[i-1][j-1]) + triangle[i][j];
+                }
+            }
         }
         
-        for(int k=0; k<triangle.length; k++) {
-            answer = Math.max(answer, arr[triangle.length-1][k]);
+        //마지막 열에서 가장 큰 값
+        for(int i=0; i<triangle.length; i++) {
+            answer = Math.max(answer, map[triangle.length-1][i]);
         }
+        
         return answer;
     }
 }

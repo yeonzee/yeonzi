160589b6ab3bf86e05dab62c72c57a1315747281
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-23 22:45:26 +0900

    [level 3] Title: 등굣길, Time: 1.02 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,38 +1,40 @@
-//문제 이상함 n,m 반대임
+//오른쪽, 아래로만 갈 수 있음
+//1,000,000,007로 나눈 나머지
 import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        
         int[][] map = new int[n+1][m+1];
         
-        //물웅덩이 -1으로 만들기
-        for(int i=0; i<puddles.length; i++) {
-            map[puddles[i][1]][puddles[i][0]] = -1;
+        //물웅덩이 -1
+        for(int[] temp:puddles) {
+            map[temp[1]][temp[0]] = -1;
         }
         
-        //집 1로 초기화
+        //집(시작) 1
         map[1][1] = 1;
         
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
                 
-                //물웅덩이 무시
+                //물웅덩이면 피하기
                 if(map[i][j] == -1) {
                     continue;
                 }
                 
-                if(map[i-1][j]>0) {  //위쪽에서 오는 값이 있다면
-                    map[i][j] += map[i-1][j];
-                }
-                
-                if(map[i][j-1]>0) {  //왼쪽에서 오는 값이 있다면
+                if(map[i][j-1] > 0) {  //왼쪽에서 올때
                     map[i][j] += map[i][j-1];
                 }
                 
-               map[i][j]  %= 1000000007;
+                if(map[i-1][j] > 0) {  //위쪽에서 올때
+                    map[i][j] += map[i-1][j];
+                }
+                
+                //효율성위해 for문 돌때마다 나눠주기
+                map[i][j] %= 1000000007;
             }
         }
+        
         return map[n][m];
     }
 }

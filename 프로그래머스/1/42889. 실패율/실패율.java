d0a13ce3a519e7e6278209648dc8339b6f84b582
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-25 00:12:43 +0900

    [level 1] Title: 실패율, Time: 83.64 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,38 +1,53 @@
+//어렵다 답보고 다시 풀음
+//ex) stages[i] = 2일 경우
+//0단계, 1단계 통과하여 2단계를 갔지만 2단계에서 실패한 것이다
+//clear의 0과 1을 늘려주고, current에서는 1단계를 늘려준다(2단계에서 실패했기 때문에 내가 현재까지 깬 스테이지는 1이다)
+//실패율은 분수이기 때문에 double형을 사용해준다
+
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
-    	// 스테이지별 도달했으나 클리어하지 못한 사람 수
-        int[] currentStages = new int[N+1];
-        // 스테이지별 도달한 사람 수
-        int[] clearStages = new int[N+1];
+        int[] answer = new int[N];
 
-        for (int i = 0; i < stages.length; i++) {
-            // 스테이지에 도달한 플레이어 수 증가
-            for (int j = 0; j < stages[i]; j++) {
-                clearStages[j] += 1;
+        //스테이지에 도달했으나 아직 클리어하지 못한 플레이어 수
+        int[] current_stage = new int[N+1];
+        
+        //스테이지에 도달한 플레이어 수
+        int[] clear_stage = new int[N+1];
+        
+        for(int i=0; i<stages.length; i++) {
+            for(int j=0; j<stages[i]; j++) {
+                clear_stage[j]++;
             }
-            // 도달 했으나 클리어하지 못한 플레이어 수 증가
-            currentStages[stages[i] - 1] += 1;
+            current_stage[stages[i]-1]++;
         }
-
-        // 스테이지 번호와 실패율을 저장할 map
-        Map<Integer, Double> map = new HashMap<>();
-
-        for (int i = 0; i < N; i++) {
-            // 스테이지에 도달한 사람이 없거나 클리어한 사람이 없는 경우
-            if (currentStages[i] == 0 || clearStages[i] == 0) {
-                map.put(i + 1, 0.0);
-            } else {
-                // 실패율 계산하여 맵에 저장
-                map.put(i + 1, (double) currentStages[i] / (double) clearStages[i]);
+        
+        //실패율을 담을 map
+        HashMap<Integer, Double> map = new HashMap<>();
+        
+        //i+1을 하는 이유: 위에서 인덱스 0부터 넣어줬기 때문에  걍 숫자 대입해보면 알게 되더라
+        for(int i=0; i<N; i++) {
+            if(current_stage[i] == 0 || clear_stage[i] == 0) {
+                map.put(i+1, 0.0);
+            }
+            else {
+                double fail = (double)current_stage[i] / (double)clear_stage[i];
+                map.put(i+1, fail);
             }
         }
-
-        List<Integer> list = new ArrayList<>(map.keySet());
-        // 실패율을 기준으로 내림차순 정렬
-        list.sort((o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
-
-        // 리스트를 배열로 변환하여 반환
-        return list.stream().mapToInt(i -> i).toArray();
+        
+        //map의 key값을 담은 list
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        
+        //value값을 기준으로 내림차순 정렬
+        //실패율이 double형이기 때문에 Double.compare 방식으로 정렬
+        Collections.sort(list, (o1, o2) -> Double.compare(map.get(o2), map.get(o1)));
+        
+        //배열로 변환
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
     }
 }

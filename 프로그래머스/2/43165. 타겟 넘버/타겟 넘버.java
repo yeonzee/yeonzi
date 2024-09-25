e91be4370e78de084787744712580e2e9b3547ac
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-25 22:10:41 +0900

    [level 2] Title: 타겟 넘버, Time: 4.15 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,30 @@
 //dfs
 import java.util.*;
 class Solution {
-    static int answer = 0;
+    static int cnt = 0;
+    static boolean[] visited;
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
-        return answer;
+        int answer = 0;
+        visited = new boolean[numbers.length];
+        
+        dfs(0, numbers, target, 0);
+        
+        return cnt;
     }
     
-    public void dfs(int[] numbers, int target, int sum, int depth) {
-        //종료 조건
-        if(depth == numbers.length) {
-            if(sum == target) {
-                answer++;
+    public void dfs(int depth, int[] numbers, int target, int num) {
+        
+        //종료조건
+        if(depth==numbers.length) {
+            if(num==target) {
+                cnt++;
             }
             return;
         }
-         
-            
+    
         //더하기
-        dfs(numbers, target, sum+numbers[depth], depth+1);
-
+        dfs(depth+1, numbers, target, num+numbers[depth]);
         //빼기
-        dfs(numbers, target, sum-numbers[depth], depth+1);
-        
+        dfs(depth+1, numbers, target, num-numbers[depth]);
     }
 }

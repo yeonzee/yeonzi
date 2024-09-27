50b98750b3ae92e69c8de08010052c9e624ab53d
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-09-27 17:22:42 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,34 @@
-//dfs
 import java.util.*;
 class Solution {
     static boolean[] visited;
-    static int answer = 0;
     public int solution(int n, int[][] computers) {
-        //방문 여부 확인
-        visited = new boolean[n+1];
+        int answer = 0;
+        visited = new boolean[n];
         
         for(int i=0; i<computers.length; i++) {
-            //방문하지 않는 노드에 대해서만 dfs
+            //방문하지 않았을 경우
             if(!visited[i]) {
-                dfs(i, computers);
-                //dfs에서 리턴돼서 오면 네트워크 하나가 생성되었다는 뜻이므로 answer++
+                dfs(computers,i);
+                //순회하고 오면 그것만큼 네트워크가 형성된 것이기 때문에 answer++;
                 answer++;
             }
         }
-        
         return answer;
     }
     
-    public void dfs(int node, int[][] computers) {
+    public void dfs(int[][] computers, int i) {
         
-        visited[node] = true;
-        for(int i=0; i<computers.length; i++) {
-            //연결되어있고 방문하지 않았다면
-            if(computers[node][i] == 1 && !visited[i]) {
-                //방문처리
-                visited[i] = true;
-                dfs(i, computers);
+        //시작 노드 방문처리
+        visited[i] = true;
+        
+        for(int j=0; j<computers[0].length; j++) {
+            if(i != j) {  //나 자신 무시
+                if(computers[i][j] == 1 && !visited[j]) {  //연결되어 있고 방문하지 않았다면 그 노드를 기점으로 탐방
+                    visited[j] = true;
+                    dfs(computers, j);
+                }
             }
         }
     }
 }
+//!visited[j] 를 넣는 이유 -> A와 B가 연결되어 있을 때 A에서 B로 갔다가 B에서 다시 A로 가는 무한 순회 발생을 피하기 위해

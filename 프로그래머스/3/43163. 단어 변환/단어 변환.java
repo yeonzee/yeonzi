544a910c7bed66527cc374e203a4c41f8175c287
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-10-01 23:29:12 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,42 @@
 //dfs
+
 import java.util.*;
 class Solution {
     static boolean[] visited;
-    static int answer;
+    static int answer = 0;
     public int solution(String begin, String target, String[] words) {
-        answer = 0;
+        
         visited = new boolean[words.length];
+        
         dfs(begin, target, words, 0);
+        
         return answer;
     }
     
-    public void dfs(String word, String target, String[] words, int depth) {
+    public void dfs(String str, String target, String[] words, int depth) {
+        
         //종료조건
-        if(word.equals(target)) {
+        if(str.equals(target)) {
             answer = depth;
             return;
         }
         
         for(int i=0; i<words.length; i++) {
-            
-            //이미 방문했다면 무시
-            if(visited[i]) {
-                continue;
-            }
-            
-            String w = words[i];
-            
-            int count = 0;
-            for(int j=0; j<w.length(); j++) {
-                if(word.charAt(j) != w.charAt(j)) {  //같지 않은게 1개만 있어야 함
-                    count++;
+            if(!visited[i]) {
+                int diff = 0;
+                for(int j=0; j<str.length(); j++) {
+                    if(str.charAt(j) != words[i].charAt(j)) {
+                        diff++;
+                    }
                 }
-            }
-            
-            if(count==1) {
-                visited[i] = true;
-                dfs(w, target, words, depth+1);
-                visited[i] = false;
+                if(diff==1) {
+                    visited[i] =true;
+                    dfs(words[i], target, words, depth+1);
+                    //백트래킹
+                    visited[i] = false;
+                }
             }
         }
         
     }
-}
+} 

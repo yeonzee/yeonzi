34918f5395a2a8a6efff1bcc7411bbe6924779fa
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-10-03 11:49:07 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,4 +1,13 @@
 //해시맵 사용
+//조합 예시
+//A->a,b 2개
+//B->c,d,e 3개
+//C->f 1개
+//2+1 = a,b 다 입는 경우 + 둘 다 안입는 경우
+//3+1 = c,d,e 다 입는 경우 + 둘 다 안입는 경우
+//1+1 = f 입는 경우 + 안입는 경우
+//마지막에 -1 = 세 종류 다 안입는 경우
+
 import java.util.*;
 class Solution {
     public int solution(String[][] clotheas) {

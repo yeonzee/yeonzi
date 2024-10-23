7bdec13cddcaaca519e9c9911d43530401d920b9
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-10-23 12:00:23 +0900

    [level 1] Title: 과일 장수, Time: 64.64 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,13 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-
+        
+        //오름차순 정렬
         Arrays.sort(score);
-
-        for(int i = score.length; i >= m; i -= m){
-            answer += score[i - m] * m;
+        
+        //큰거부터 넣어야 하기 때문에 뒤에서 부터
+        //i>=m -> 남은 사과가 한 상자에 담을 사과보다 많아야 함
+        for(int i=score.length; i>=m; i-=m) {
+            answer += score[i-m] * m;
         }
-
+    
         return answer;
     }
 }

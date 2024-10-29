dc3ce5e2445a7caf3a53360d25773fd63bb0a902
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-10-29 23:12:47 +0900

    [level 2] Title: 롤케이크 자르기, Time: 333.10 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,36 +1,40 @@
-/*
-중복이 없다 생각하여 set로 풀었더니 시간오류 이슈
-map으로 풀어야 함
-*/
+//중복을 허용하지 않는 set 2개 만들기
+//라고 생각했지만 시간 이슈 생기네....
+//map으로 풀기
+
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
-        int answer = -0;
+        int answer = 0;
+        HashMap<Integer, Integer> map_1 = new HashMap<>();
+        HashMap<Integer, Integer> map_2 = new HashMap<>();
         
-        
-        HashMap<Integer, Integer> map_1 = new HashMap<>(); //형
-        HashMap<Integer, Integer> map_2 = new HashMap<>(); //동생
-        
-        //형에 다 넣고 빼가면서 계산
-        for (int num:topping) {
+        //형한테 몰아서 넣기
+        for(int num:topping) {
             map_1.put(num, map_1.getOrDefault(num,0)+1);
         }
         
-        for (int n:topping) {
-            //동생에 넣기
+        //동생한테 넣고 형한테 빼가면서 갯수 확인하기
+        for(int n:topping) {
             map_2.put(n, map_2.getOrDefault(n,0)+1);
-            
-            if (map_1.get(n)-1 == 0) {
+        
+            //map_1.get(n)-1이 0개라면 1개의 토핑이 남아있다는 뜻 -> 동생한테 줬기 때문에 형한테서 삭제해야 함
+            if(map_1.get(n)-1 == 0) {
                 map_1.remove(n);
             }
+            
+            //토핑의 갯수가 1개 이상인 경우
+            //동생한테 들고갔기 때문에 형한테서 -1
             else {
-                map_1.put(n, map_1.get(n)-1);
+                map_1.put(n,map_1.get(n)-1);
             }
             
-            if (map_1.size() == map_2.size()) {
+            //형이랑 동생 갯수 확인
+            if(map_1.size() == map_2.size()) {
                 answer++;
             }
         }
+        
         return answer;
     }
 }

2be0cb7ae43d31b4148ab67ce60a9be530f7e481
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-11-06 00:28:23 +0900

    [level 2] Title: 구명보트, Time: 13.06 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,34 +1,31 @@
-//어떠한 방법을 써도 풀리지 않음
-//투포인터 방법
+//정렬한 후 앞 뒤로 구명보트에 담기
+//문제를 제대로 읽자 한 번에 최대 2명 탈 수 있음
 import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        
+    
         int i = 0;
         int j = people.length-1;
         
-        int count = 0;
-        
         while(true) {
-            if(i > j) {
+            //종료조건
+            if(i>j) {
                 break;
             }
             
-            int sum = 0;
-            sum = people[i] + people[j];
+            int sum = people[i] + people[j];
             
-            if(sum > limit) {  //구명보트 제한을 넘는다면 뒤에 한 사람만 처리
-                j--;
-                count++;
-            }
-            else {  //구명보트에 2명 채워서 처리
+            if(sum <= limit) {
                 i++;
                 j--;
-                count++;
             }
+            else {
+                j--;
+            }
+            answer++;
         }
-        return count;
+        return answer;
     }
 }

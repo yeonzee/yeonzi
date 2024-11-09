660aa17cf480882de2930f2d813411df12cef78c
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-11-09 18:33:13 +0900

    [level 2] Title: 프로세스, Time: 1.00 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,27 +2,27 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        //우선순위 큐는 작은 것부터 나오므로 역순
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
         
-        //큐에 넣기
-        for (int num:priorities) {
-            queue.offer(num);
+        //큰 수부터 꺼내기 위해 우선순위큐를 내림차순
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int num:priorities) {
+            q.offer(num);
         }
         
-        while(!queue.isEmpty()) {
-            for (int i=0;i<priorities.length;i++) {
-                //큐의 가장 앞에 있는 값과 priorites[i]의 값이 같다면
-                if(queue.peek() == priorities[i]) {
-                    //삭제
-                    queue.poll();
+        while(!q.isEmpty()) {
+            for(int i=0; i<priorities.length; i++) {
+                //큐의 가장 앞에 있는 값과 같다면
+                if(q.peek() == priorities[i]) {
+                    q.poll();
                     answer++;
                     
-                    //내가 찾던 위치와 같다면 return
-                    if (location == i) {
+                    //location과 일치하다면
+                    if(i==location) {
                         return answer;
                     }
                 }
+                
             }
         }
         return answer;

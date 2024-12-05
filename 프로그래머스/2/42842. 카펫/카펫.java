120f0cdf07c82e8e4162ea428edecc0efc748b88
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-12-05 22:42:18 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,19 @@
+//시간 효율성을 위해 for문은 1개만
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for(int i=1; i<=yellow; i++) {
-            //정수일경우만 고려
-            if(yellow%i==0) {
-                int j = yellow/i;
+        for(int i=3; i<brown; i++) {
+            
+            //나누어 딱 떨어지는 것만 계산
+            if((brown+yellow)%i==0) {
+                int j=(brown+yellow)/i;
                 
-                if((i*2+j*2)+4 == brown) {
-                    answer[0] = Math.max(i,j)+2;
-                    answer[1] = Math.min(i,j)+2;
-                    return answer;
-                }
+                if((i-2)*2+(j-2)*2+4 == brown) {
+                answer[0] = Math.max(i,j);
+                answer[1] = Math.min(i,j);
+            }
             }
             
             

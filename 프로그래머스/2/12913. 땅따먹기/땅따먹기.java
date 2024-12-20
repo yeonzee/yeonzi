79f096b42df47dde58bba4a74c41568115f92b70
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-12-21 02:29:59 +0900

    [level 2] Title: 땅따먹기, Time: 21.56 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,20 +1,21 @@
-//dp
+//같은 열을 제외한 가장 큰 값을 누적
 import java.util.*;
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
 
-        for (int i=1; i<land.length;i++) {
-            land[i][0] += Math.max(land[i-1][1],Math.max(land[i-1][2],land[i-1][3]));
-            land[i][1] += Math.max(land[i-1][0],Math.max(land[i-1][2],land[i-1][3]));
-            land[i][2] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][3]));
-            land[i][3] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][2]));
+        for(int i=1; i<land.length; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2],land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2],land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1],land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1],land[i-1][2]));
         }
         
-        for (int i=0; i<4 ; i++) {
-            answer = Math.max(answer, land[land.length-1][i]);
+        //마지막 행에서 가장 큰 값
+        for(int j=0; j<4; j++) {
+            answer = Math.max(answer, land[land.length-1][j]);
         }
-        
+
         return answer;
     }
 }

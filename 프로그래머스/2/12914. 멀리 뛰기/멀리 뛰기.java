49623aaf23f4f7695da1516d9954bd16012ed025
yawnzi <125468560+yawnzi@users.noreply.github.com>
2024-12-25 14:13:42 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,18 +1,21 @@
+//2->2, 3->3, 4->5, 5->8
 //피보나치
+//재귀로 구현하면 시간 초과
 import java.util.*;
 class Solution {
     public long solution(int n) {
         long answer = 0;
-        int[] arr = new int[2001];
         
-        arr[1] = 1;
-        arr[2] = 2;
+        //n+1했더니 런타임오류가 뜸 -> 메모리 최적화 오류 발생...?
+        long[] a = new long[2001];
+        
+        a[1] = 1;
+        a[2] = 2;
         
         for(int i=3; i<=n; i++) {
-            arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
+            a[i] = (a[i-1] + a[i-2])%1234567;
         }
         
-        
-        return arr[n];
+        return a[n];
     }
 }

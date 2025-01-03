3aa715b05ba51871960b808d1cd181400730efe9
zoo <125468560+yawnzi@users.noreply.github.com>
2025-01-04 00:15:02 +0900

    [level 2] Title: 피로도, Time: 3.45 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,30 +1,33 @@
 //dfs
 import java.util.*;
 class Solution {
-    static boolean[] visited;
-    static int answer = 0;
+    static int answer;
+    static boolean[] checked;
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
+        answer = 0;
+        checked = new boolean[dungeons.length];
         
         dfs(0, dungeons, k);
-        
+            
         return answer;
     }
     
     public void dfs(int depth, int[][] dungeons, int k) {
         
-        for(int j=0; j<dungeons.length; j++) {
-            if(!visited[j] && k>=dungeons[j][0]) {
-                visited[j] = true;
-                k -= dungeons[j][1];
+        for(int i=0; i<dungeons.length; i++) {
+            //조건을 충족한다면 탐험
+            if(k>=dungeons[i][0] && !checked[i]) {
+                //방문처리
+                checked[i] = true;
+                k -= dungeons[i][1];
                 dfs(depth+1, dungeons, k);
                 
                 //백트래킹
-                visited[j] = false;
-                k += dungeons[j][1];
+                checked[i] = false;
+                k += dungeons[i][1];
             }
         }
         
-        answer = Math.max(answer, depth);
+        answer = Math.max(answer,depth);
     }
 }

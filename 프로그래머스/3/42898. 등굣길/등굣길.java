9ac97bea87523940074e2818d445a9d6eaafc2cc
zoo <125468560+yawnzi@users.noreply.github.com>
2025-01-17 15:43:58 +0900

    [level 3] Title: 등굣길, Time: 0.99 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,40 +1,36 @@
-//오른쪽, 아래로만 갈 수 있음
-//1,000,000,007로 나눈 나머지
+//오른쪽과 아래쪽으로만 이동 / 1000000007로 나눈 나머지
+//해당 칸에 접근할 수 있는 방법 갯수 누적하기
 import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
         int[][] map = new int[n+1][m+1];
+        int mod = 1000000007;
         
         //물웅덩이 -1
         for(int[] temp:puddles) {
             map[temp[1]][temp[0]] = -1;
         }
         
-        //집(시작) 1
+        //시작
         map[1][1] = 1;
         
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
-                
-                //물웅덩이면 피하기
-                if(map[i][j] == -1) {
-                    continue;
-                }
-                
-                if(map[i][j-1] > 0) {  //왼쪽에서 올때
-                    map[i][j] += map[i][j-1];
-                }
-                
-                if(map[i-1][j] > 0) {  //위쪽에서 올때
-                    map[i][j] += map[i-1][j];
-                }
-                
-                //효율성위해 for문 돌때마다 나눠주기
-                map[i][j] %= 1000000007;
-            }
-        }
-        
-        return map[n][m];
+       for(int i=1; i<=n; i++) {
+           for(int j=1; j<=m; j++) {
+               //물웅덩이 만나면 패스
+               if(map[i][j] == -1) {
+                   continue;
+               }
+               //아래로 이동
+               if(map[i-1][j] > 0) {
+                   map[i][j] += map[i-1][j] % mod;
+               }
+               //오른쪽으로 이동
+               if(map[i][j-1] > 0) {
+                   map[i][j] += map[i][j-1] % mod;
+               }
+           }
+       } 
+        return map[n][m] % mod;
     }
 }

3c214ec86bec4c6900802fcdbb25387432a7f5dc
zoo <125468560+yawnzi@users.noreply.github.com>
2025-01-17 14:45:16 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162#qna) 
 
 ### 성능 요약
 
-메모리: 81.7 MB, 시간: 0.23 ms
+메모리: 84.1 MB, 시간: 0.26 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 27일 17:22:38
+2025년 01월 17일 14:45:12
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,15 +1,17 @@
+//dfs
 import java.util.*;
 class Solution {
-    static boolean[] visited;
+    static boolean[] check;
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n];
+        check = new boolean[n];
         
         for(int i=0; i<computers.length; i++) {
             //방문하지 않았을 경우
-            if(!visited[i]) {
-                dfs(computers,i);
-                //순회하고 오면 그것만큼 네트워크가 형성된 것이기 때문에 answer++;
+            if(!check[i]) {
+                dfs(computers, i);
+                
+                //dfs 순회하고 온 만큼 네트워크 형성
                 answer++;
             }
         }
@@ -18,17 +20,14 @@ class Solution {
     
     public void dfs(int[][] computers, int i) {
         
-        //시작 노드 방문처리
-        visited[i] = true;
+        //방문처리
+        check[i] = true;
         
-        for(int j=0; j<computers[0].length; j++) {
-            if(i != j) {  //나 자신 무시
-                if(computers[i][j] == 1 && !visited[j]) {  //연결되어 있고 방문하지 않았다면 그 노드를 기점으로 탐방
-                    visited[j] = true;
-                    dfs(computers, j);
-                }
+        for(int j=0; j<computers.length; j++) {
+            //나 자신이 아니고 && 연결되어있고 && 방문하지 않았을 때
+            if(i!=j && computers[i][j] == 1 && !check[j]) {
+                dfs(computers, j);
             }
         }
     }
 }
-//!visited[j] 를 넣는 이유 -> A와 B가 연결되어 있을 때 A에서 B로 갔다가 B에서 다시 A로 가는 무한 순회 발생을 피하기 위해

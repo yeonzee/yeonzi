2164455dbdfeca8ee94b1c6e08adaabf69cb4b34
zoo <125468560+yawnzi@users.noreply.github.com>
2025-01-25 14:47:45 +0900

    [level 2] Title: 삼각 달팽이, Time: 18.30 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,36 +1,56 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(n*(n+1))/2];
-        int[][] matrix = new int[n][n];
-
-        int x = -1, y = 0;
+        
+        int[][] arr = new int[n][n];
+        
         int num = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) { 	
-                if (i % 3 == 0) {
+        
+        int x = -1;
+        int y = 0;
+        
+        for(int i=0; i<n; i++) {
+            
+            //아래
+            if(i%3==0) {
+                for(int j=i; j<n; j++) {
                     x++;
-                } else if (i % 3 == 1) {
+                    num++;
+                    arr[x][y] = num;
+                }
+            }
+            
+            //오른쪽
+            else if(i%3==1) {
+                for(int j=i; j<n; j++) {
                     y++;
-                } else if (i % 3 == 2) {
+                    num++;
+                    arr[x][y] = num;
+                }
+            }
+            
+            //대각선 위
+            else if(i%3==2) {
+                for(int j=i; j<n; j++) {
                     x--;
                     y--;
+                    num++;
+                    arr[x][y] = num;
                 }
-                num++;
-                matrix[x][y] = num;
             }
         }
         
-        int idx=0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<=i;j++){
-                answer[idx] = matrix[i][j];
+        int[] answer = new int[num];
+        
+        //answer에 넣기
+        int idx = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<=i; j++) {
+                answer[idx] = arr[i][j];
                 idx++;
             }
         }
-
+        
         return answer;
     }
 }

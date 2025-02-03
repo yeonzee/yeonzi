ad73691e449e0b6c204548994117113394a70f33
zoo <125468560+yawnzi@users.noreply.github.com>
2025-02-04 02:01:54 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -2,25 +2,30 @@ import java.util.*;
 class Solution {
     public int solution(String[][] board, int h, int w) {
         int answer = 0;
-        int count = 0;
+        
         int row = board.length;
         int column = board[0].length;
         
-        if (h>0 && board[h-1][w].equals(board[h][w])) {
-        count++;
+        //위
+        if(h > 0 && board[h-1][w].equals(board[h][w])) {
+            answer++;
         }
         
-        if (w>0 && board[h][w-1].equals(board[h][w])) {
-        count++;
+        //아래
+        if(h < row-1 && board[h+1][w].equals(board[h][w])) {
+            answer++;
         }
         
-        if (h<row-1 && board[h+1][w].equals(board[h][w])) {
-            count++;
-        }
-        if (w<column-1 && board[h][w+1].equals(board[h][w])) {
-            count++;
+        //왼쪽
+        if(w > 0 && board[h][w-1].equals(board[h][w])) {
+            answer++;
         }
         
-        return count;
+        //오른쪽
+        if(w < column-1 && board[h][w+1].equals(board[h][w])) {
+            answer++;
+        }
+        
+        return answer;
     }
 }

c9d387addc4d041e4a4797a098d47581bcf344e1
zoo <125468560+yawnzi@users.noreply.github.com>
2025-02-07 19:26:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.01 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,15 +1,16 @@
 //최단거리 bfs
 import java.util.*;
 class Solution {
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {-1,1,0,0};
     static int[][] visited;
+    
     public int solution(int[][] maps) {
-        int answer = 0;
         visited = new int[maps.length][maps[0].length];
         
         bfs(maps);
         
+        //도달하지 못하면 -1
         if(visited[maps.length-1][maps[0].length-1] == 0) {
             return -1;
         }
@@ -18,26 +19,29 @@ class Solution {
     }
     
     public void bfs(int[][] maps) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {0,0});  //시작
+        Queue<int[]> q = new LinkedList<>(); 
+        //시작 넣기
+        q.offer(new int[] {0,0});
+        
+        //시작 방문 처리
         visited[0][0] = 1;
         
         while(!q.isEmpty()) {
             int[] temp = q.poll();
-            int now_x = temp[0];
-            int now_y = temp[1];
+            int x = temp[0];
+            int y = temp[1];
             
             for(int i=0; i<4; i++) {
-                int next_x = now_x + dx[i];
-                int next_y = now_y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                //무시: 범위 벗어남, 갈 수 없는 길임, 이미 방문했음
-                if(next_x < 0 || next_x > maps.length-1 || next_y < 0 || next_y > maps[0].length-1 || maps[next_x][next_y] == 0 || visited[next_x][next_y] != 0) {
+                //범위를 벗어나거나 벽이거나 이미 방문했다면 continue
+                if(nx<0 || nx>maps.length-1 || ny<0 || ny>maps[0].length-1 || maps[nx][ny] == 0 || visited[nx][ny] != 0) {
                     continue;
                 }
                 else {
-                    visited[next_x][next_y] = visited[now_x][now_y] + 1;
-                    q.add(new int[] {next_x, next_y});
+                    visited[nx][ny] = visited[x][y] + 1;
+                    q.add(new int[] {nx,ny});
                 }
                 
             }

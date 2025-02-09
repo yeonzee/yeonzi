7581d62c15a257781a3cd0b41662db4cb210c0f9
zoo <125468560+yawnzi@users.noreply.github.com>
2025-02-10 02:51:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 73.38 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -6,37 +6,40 @@ class Solution {
         
         for(int i=0; i<s.length(); i++) {
             String str = s.substring(i) + s.substring(0,i);
-            Stack<Character> stack = new Stack<>();
             
-            //시작이 닫힌 괄호일 경우 올바르지 않은 괄호식
-            if(str.charAt(0) == ']' || str.charAt(0) == '}' || str.charAt(0) == ')') {
+            //시작이 닫힌 괄호이면 x
+            if(str.charAt(0) == ']' || str.charAt(0) == ')' || str.charAt(0) == '}') {
                 continue;
             }
             
-            for(int j=0; j<str.length(); j++) {
-                char c = str.charAt(j);
+            Stack<Character> stack = new Stack<>();
+            stack.push(str.charAt(0));
+            
+            for(int j=1; j<str.length(); j++) {
                 
-                //열린괄호
-                if(c == '{' || c == '[' || c == '(') {
-                    stack.push(c);
+                //열린 괄호
+                if(str.charAt(j) == '(' || str.charAt(j) == '[' || str.charAt(j) == '{') {
+                    stack.push(str.charAt(j));
                 }
+                
                 //올바른 괄호
-                else if(!stack.isEmpty() && c == '}' && stack.peek() == '{') {
-                    stack.pop();
+                else if(!stack.isEmpty() && stack.peek() == '(' && str.charAt(j) == ')') {
+                   stack.pop(); 
                 }
-                else if(!stack.isEmpty() && c == ']' && stack.peek() == '[') {
-                    stack.pop();
+                else if(!stack.isEmpty() && stack.peek() == '{' && str.charAt(j) == '}') {
+                   stack.pop(); 
                 }
-                else if(!stack.isEmpty() && c == ')' && stack.peek() == '(') {
-                    stack.pop();
+                else if(!stack.isEmpty() && stack.peek() == '[' && str.charAt(j) == ']') {
+                   stack.pop(); 
                 }
             }
             
-            //스택이 비어있으면 올바른 괄호
+            //stack이 비어있다면 올바른 괄호 문자열
             if(stack.isEmpty()) {
                 answer++;
             }
         }
+        
         return answer;
     }
 }

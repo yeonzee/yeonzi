54e397478118da75f8d3d48413ac2f11eae87759
zoo <125468560+yeonzee@users.noreply.github.com>
2025-02-20 10:20:42 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 3.14 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,29 +1,29 @@
-// 1. n을 문자열로 변경
-// 2. 문자배열로 저장
-// 3. sort 하여 내림차순 배열
-// 4. 정리된 배열을 다시 문자열로 변환
-// 5. long으로 변환
-
+/*
+String으로 변경
+배열에 임시 저장 후 내림차순 정렬
+Long형으로 변환
+*/
 import java.util.*;
 class Solution {
     public long solution(long n) {
         long answer = 0;
         
         String str = String.valueOf(n);
+        int len = str.length();
+        Character[] temp = new Character[len];
         
-        Character[] a = new Character[str.length()];
-        
-        for (int i=0;i<str.length();i++) {
-            a[i] = str.charAt(i);
+        for(int i=0; i<len; i++) {
+            temp[i] = str.charAt(i);
         }
         
-        Arrays.sort(a,Collections.reverseOrder());
+        //내림차순 정렬
+        Arrays.sort(temp, Collections.reverseOrder());
         
-        StringBuilder b = new StringBuilder(a.length);
-        for (char c:a) {
-            b.append(c);
+        String result = "";
+        for(int j=0; j<len; j++) {
+            result += temp[j];
         }
         
-        return Long.parseLong(b.toString());
+        return Long.parseLong(result);
     }
 }

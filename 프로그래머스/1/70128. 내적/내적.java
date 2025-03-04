419b593d037d5e3dabcac6e44eadc91670305c64
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-04 10:20:11 +0900

    [level 1] Title: 내적, Time: 0.05 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,9 +1,9 @@
+import java.util.*;
 class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
         
-        // a[i] * b[i]를 answer에 더하기
-        for (int i = 0;i<a.length;i++) {
+        for(int i=0; i<a.length; i++) {
             answer += a[i] * b[i];
         }
         return answer;

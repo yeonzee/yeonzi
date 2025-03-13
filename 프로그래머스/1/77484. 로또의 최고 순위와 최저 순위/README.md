66a5d4dfdf89960530a1755bf1869c47dc85de6e
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-13 10:46:30 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.41 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484#qna) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 0.40 ms
+메모리: 78.5 MB, 시간: 0.41 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 15:31:59
+2025년 03월 13일 10:46:25
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,27 +1,46 @@
+//최고 순위 -> 0을 다 맞는 숫자로 바꾸기
+//최저 순위 -> 0을 다 안맞는 숫자로 바꾸기
 import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
         
+        //정렬
         Arrays.sort(lottos);
         Arrays.sort(win_nums);
         
-        int count_good = 0;
-        int count_bad = 0;
-        
-        for (int a:lottos) {
-            if (a==0) {
-                count_good++;
-            }
-            for (int b:win_nums) {
-                if (a==b) {
-                    count_good++;
-                    count_bad++;
+        //기존에 맞춘 번호 찾기
+        int cnt = 0;
+        for(int i=0; i<lottos.length; i++) {
+            for(int j=0; j<win_nums.length; j++) {
+                if(lottos[i] == win_nums[j]) {
+                    cnt++;
+                    break;
                 }
-                
             }
         }
-        int[] answer = {Math.min(7 - count_good, 6), Math.min(7 - count_bad, 6)};
+        
+        answer[1] = 7-cnt;
+        
+        //1개 맞춰도, 하나도 못맞춰도 6등
+        if(cnt == 0 || cnt == 1) {
+            answer[1] = 6;
+        }
+        
+        //0찾기
+        for(int n:lottos) {
+            if(n==0) {
+                cnt++;
+            }
+        }
+        
+        answer[0] = 7-cnt;
+        
+        //1개 맞춰도, 하나도 못맞춰도 6등
+        if(cnt == 0 || cnt == 1) {
+            answer[0] = 6;
+        }
+        
         return answer;
-
     }
 }

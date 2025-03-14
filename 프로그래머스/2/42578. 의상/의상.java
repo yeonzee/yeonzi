75136a6c6d6f04db5db341ae99a3d8d261c5d43b
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-14 10:54:26 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,28 +1,19 @@
-//해시맵 사용
-//조합 예시
-//A->a,b 2개
-//B->c,d,e 3개
-//C->f 1개
-//2+1 = a,b 다 입는 경우 + 둘 다 안입는 경우
-//3+1 = c,d,e 다 입는 경우 + 둘 다 안입는 경우
-//1+1 = f 입는 경우 + 안입는 경우
-//마지막에 -1 = 세 종류 다 안입는 경우
-
 import java.util.*;
 class Solution {
-    public int solution(String[][] clotheas) {
+    public int solution(String[][] clothes) {
         int answer = 1;
         
         HashMap<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<clotheas.length; i++) {
-            map.put(clotheas[i][1], map.getOrDefault(clotheas[i][1],0)+1);
+        for(String[] temp:clothes) {
+            map.put(temp[1], map.getOrDefault(temp[1],0)+1);  
         }
         
-        //조합
         for(String str:map.keySet()) {
             answer *= map.get(str)+1;
         }
+        
+        //아무것도 안입는 경우 제외
         return answer-1;
     }
 }

3b03a936e6dd9feb92300fb706f0be3bcb83529d
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-18 10:54:14 +0900

    [level 2] Title: 프로세스, Time: 1.16 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,28 +3,29 @@ class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        //큰 수부터 꺼내기 위해 우선순위큐를 내림차순
+        //우선순위 큐 내림차순으로 정렬
         PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
         
         for(int num:priorities) {
-            q.offer(num);
+            q.add(num);
         }
         
         while(!q.isEmpty()) {
-            for(int i=0; i<priorities.length; i++) {
-                //큐의 가장 앞에 있는 값과 같다면
+           for(int i=0; i<priorities.length; i++) {
+               //우선순위대로 비교
                 if(q.peek() == priorities[i]) {
                     q.poll();
                     answer++;
                     
-                    //location과 일치하다면
+                    //location과 같다면
                     if(i==location) {
                         return answer;
                     }
+
                 }
-                
-            }
+            } 
         }
+            
         return answer;
     }
 }

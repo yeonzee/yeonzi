d857e57f64295f749c29e360986da8d42ac22a3e
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-21 10:58:21 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 14.45 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,33 +1,52 @@
+//int는 런타임오류 발생 -> Long형으로 변환
 import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        //n을 k진법으로 변환
-        String str = Integer.toString(n,k);
         
-        //0을 기준으로 분리하기
-        String[] parts = str.split("0");
+        ArrayList<Integer> list = new ArrayList<>();
+        while(n > 0) {
+            list.add(n%k);
+            n /= k;
+        }
         
-        for (String part : parts) {
-            // 빈 문자열 처리
-            if (!part.isEmpty()) {
-                long a = Long.parseLong(part);
-                if (a > 1) {
-                    boolean isPrime = true;
-                    // 소수 판별
-                    for (int j = 2; j <= Math.sqrt(a); j++) {
-                        if (a % j == 0) {
-                            isPrime = false;
-                            break;
-                        }
-                    }
-                    if (isPrime) {
-                        answer++;
+        String str = "";
+        for(int i=list.size()-1; i>=0 ; i--) {
+            str += list.get(i);
+        }
+        
+        //0을 기준으로 나누기
+        String[] temp = str.split("0");
+        
+        for(String s:temp) {
+            //빈 문자열 처리
+            if(s.equals("")) {
+                continue;
+            }
+            
+            //소수 판별
+            long num = Long.parseLong(s);
+            
+            boolean check = true;
+            
+            //1보다 큰 경우만 판단
+            if(num > 1) {
+                for(int i=2; i<=Math.sqrt(num); i++) {
+                    //소수가 아님
+                    if(num % i == 0) {
+                       check = false;
+                        break;
                     }
                 }
+                //소수가 맞다면 +1
+                if(check) {
+                    answer++;
+                }
             }
+            
+            
         }
-
+        
         return answer;
     }
 }

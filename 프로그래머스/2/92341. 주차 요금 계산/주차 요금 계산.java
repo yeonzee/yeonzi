46446f4900947dd54813a9a6a92d619a37888274
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-24 23:31:52 +0900

    [level 2] Title: 주차 요금 계산, Time: 7.48 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,64 +1,61 @@
-/*
-차량의 입차 정보를 저장할 map, 출차시 이용한 시간을 저장할 map
-이용 시간 map에서 차량번호로 오름차순
-요금계산 -> 올림해야함!
-*/
 import java.util.*;
 class Solution {
     public int[] solution(int[] fees, String[] records) {
+
+        //입차 정보를 담을 map
+        HashMap<String, String> in = new HashMap<>();
         
-        //차량 입/출차 정보 저장할 map
+        //차량별 주차 시간을 담을 map
         HashMap<String, Integer> map = new HashMap<>();
         
-        //차량들이 입/출차한 시간 및 요금 기록할 map
-        HashMap<String, Integer> result = new HashMap<>();
-    
         for(String str:records) {
             String[] temp = str.split(" ");
-            String t = temp[0];
-            String number = temp[1];
-            String state = temp[2];
+            String time = temp[0];
+            String car = temp[1];
+            String status = temp[2];
             
-            String[] time_cal = t.split(":");
-            int time = Integer.parseInt(time_cal[0])*60 + Integer.parseInt(time_cal[1]);
-            
-            //입차인 경우
-            if(state.equals("IN")) {
-                map.put(number, time);
+            //입차인 경우 입차 map에 담기
+            if(status.equals("IN")) {
+                in.put(car, time);
             }
-            //출차인 경우
+            //출차인 경우 시간 계산 + 입차 map에서 삭제
             else {
-                int total_t = time - map.get(number);
-                result.put(number, result.getOrDefault(number, 0) + total_t);
+                String[] temp_out = time.split(":");
+                int out_time = Integer.parseInt(temp_out[0])*60 + Integer.parseInt(temp_out[1]);
                 
-                //출차했으므로 map에서 지워주기
-                map.remove(number);
+                String[] temp_in = in.get(car).split(":");
+                int in_time = Integer.parseInt(temp_in[0])*60 + Integer.parseInt(temp_in[1]);
+                
+                map.put(car, map.getOrDefault(car,0)+(out_time-in_time));
+                
+                //입차 map에서 삭제
+                in.remove(car);
             }
         }
         
-        //입차만 하고 출차x
-        //23:59을 출차시간으로 계산 -> 1439
-        for(String s:map.keySet()) {
-            result.put(s, result.getOrDefault(s,0) + (1439-map.get(s)));
+        //마지막까지 출차하지 않은 차 계산
+        for(String s:in.keySet()) {
+            String[] temp_in = in.get(s).split(":");
+            int in_time = Integer.parseInt(temp_in[0])*60 + Integer.parseInt(temp_in[1]);
+            map.put(s, map.getOrDefault(s,0)+(1439-in_time));
         }
         
-        // 차량 번호 정렬을 위한 리스트 생성
-        List<String> carList = new ArrayList<>(result.keySet());
-        Collections.sort(carList); // 차량 번호 기준 오름차순 정렬
-
-        int[] answer = new int[carList.size()];
+        //오름차순 정렬
+        ArrayList<String> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
         
-        int index=0;
+        int[] answer = new int[list.size()];
+        int idx = 0;
         //요금 계산
-        for(String car:carList) {
-            //기본요금
-            if(result.get(car) <= fees[0]) {
-                answer[index] = fees[1];
+        for(String s:list) {
+            //기본시간 보다 작다면
+            if(map.get(s) <= fees[0]) {
+                answer[idx] = fees[1];
             }
             else {
-                answer[index] = fees[1] + (int) Math.ceil((double) (result.get(car) - fees[0]) / fees[2]) * fees[3];
+                answer[idx] = (int)((Math.ceil((double)(map.get(s) - fees[0]) / fees[2])) * fees[3]) + fees[1];
             }
-            index++;
+            idx++;
         }
         
         return answer;

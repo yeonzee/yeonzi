2d750fbfff62b5d5fa67bfb9771b0b3cdcceb321
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-25 12:04:37 +0900

    [level 2] Title: 모음 사전, Time: 6.03 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,11 +1,9 @@
-//재귀를 사용해야만 할 것 같으니 dfs
-//StringBuilder을 사용하면 더 효율적
-//String을 통해 풀 수도 있지만 지금은 깊이가 5여서 가능한거임. 더 커지면 메모리 효율성 안좋아짐
 import java.util.*;
 class Solution {
-    static int cnt = 0;
     static String[] alpha = {"A","E","I","O","U"};
+    static int cnt = 0;
     static int answer = 0;
+    static boolean fin = false;
     public int solution(String word) {
         
         dfs(word, "", 0);
@@ -14,8 +12,14 @@ class Solution {
     }
     
     public void dfs(String word, String w, int depth) {
+        //효율성을 위해 추가적인 재귀함수 호출x
+        if(fin) {
+            return;
+        }
+        
         if(w.equals(word)) {
             answer = cnt;
+            fin = true;
             return;
         }
         
@@ -26,7 +30,6 @@ class Solution {
         for(int i=0; i<alpha.length; i++) {
             cnt++;
             dfs(word, w+alpha[i], depth+1);
-            //백트래킹이 필요 없는 이유: dfs부를 때 새로운 문자열을 만든 것이기 때문에 본 문자열의 변화는 없다. 그래서 백트래킹으로 원본으로 돌릴 필요 없다.
         }
     }
 }

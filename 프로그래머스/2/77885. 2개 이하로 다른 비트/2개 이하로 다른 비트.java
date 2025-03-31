a1ec4b21949d824efe1f2cbdfc2bb2c654324122
zoo <125468560+yeonzee@users.noreply.github.com>
2025-03-31 16:06:37 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 127.84 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,39 +1,41 @@
 /*
-1.짝수일때는 끝자리가 0이므로 원래 숫자에서 1을 더해주기 (비트가 1개 바뀐 경우)
-2.홀수일때는 1로만 이루어졌는지 0도 있는지 확인
-3.1로만 이루어져있는 경우: 맨 앞에 10을 추가하고 비트 자리수-1의 1을 채워주기
-4.0도 있는 경우: 끝에서부터 처음 0이 나오는 곳에 10을 집어넣기
+1. 짝수일 경우 맨 뒷자리는 무조건 0, 그러므로 +1만 해주면 됨
+2. 홀수일 경우 뒤에서 0이 시작되는 인덱스를 10으로 바꿔주면 됨
+3. 홀수인데 0이 없는 경우는 맨 앞에 10을 붙여주면됨
+이런식으로 하면 비트가 다른 지점이 2개 이하이면서 가장 작은 수 완성
 */
 import java.util.*;
 class Solution {
     public long[] solution(long[] numbers) {
         long[] answer = new long[numbers.length];
         
-        for(int i=0;i<numbers.length;i++) {
+        for(int i=0; i<numbers.length; i++) {
             long num = numbers[i];
-            //짝수라면
+            
+            //짝수라면 +1
             if(num % 2 == 0) {
                 answer[i] = num+1;
             }
             //홀수라면
             else {
-                String s = Long.toString(num,2);
-                //마지막으로 0이 오는 위치
-                int idx = s.lastIndexOf("0");
-                    
-                //0이 있다면
+                //2진수로 변환
+                String str = Long.toString(num, 2);
+                
+                //0의 인덱스 확인
+                int idx = str.lastIndexOf("0");
+                
+                //0이 있는 홀수라면
                 if(idx != -1) {
-                    s = s.substring(0,idx) + "10" + s.substring(idx+2);
-                    answer[i] = Long.parseLong(s,2);
+                    str = str.substring(0, idx) + "10" + str.substring(idx+2);
                 }
-                //0이 없다면
+                //0이 없는(1로만 채워진) 홀수라면
                 else {
-                    s = "10" + s.substring(1);
-                    answer[i] = Long.parseLong(s,2);
+                    str = "10" + str.substring(1);
                 }
+                
+                answer[i] = Long.parseLong(str,2);
             }
         }
-        
         return answer;
     }
 }

4ac1b97cd02908380947d0ddc3cdf0258b8affd4
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-03 12:03:25 +0900

    [level 2] Title: 소수 찾기, Time: 27.71 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,49 +1,53 @@
 import java.util.*;
 class Solution {
-    //중복제거
     static HashSet<Integer> set = new HashSet<>();
     public int solution(String numbers) {
         int answer = 0;
         
+        //수 만들기
         recursive("", numbers);
         
-        
-        boolean[] check = new boolean[set.size()];
-        //소수인지 확인
-        int idx = 0;
-        for(int num:set) {
-            if(num==0 || num==1) {
-                check[idx] = false;
-            }
-            else {
-                boolean isprime = true;
-                for(int j=2; j<=Math.sqrt(num); j++) {
-                    if(num % j == 0) {  //소수가 아님
-                        isprime = false;
-                        break;
-                    }
-                }
-                check[idx] = isprime;
-            }
-            idx++;
-        }
-        
-        for(int i=0; i<check.length; i++) {
-            if(check[i]) {
+        //소수 판별
+        for(int n:set) {
+            //소수라면
+            if(prime(n)) {
                 answer++;
             }
         }
         
+        //확인용
+        // System.out.println("numbers: " + set);
+        // for(int n:set) {
+        //     System.out.println("Checking: " + n + " -> " + prime(n));
+        // }
+        
         return answer;
     }
     
+    //수 만들기
     public void recursive(String s, String others) {
+        //비어있지 않은 문자열이라면 set에 넣기
         if(!s.equals("")) {
             set.add(Integer.valueOf(s));
         }
         
+        //재귀
         for(int i=0; i<others.length(); i++) {
             recursive(s+others.charAt(i), others.substring(0,i)+others.substring(i+1));
         }
+    }
+    
+    public boolean prime(int n) {
+        if(n == 0 || n == 1) {
+            return false;
+        }
+
+        for(int j = 2; j <= Math.sqrt(n); j++) {
+            if(n % j == 0) {
+                return false;  //약수가 발견되면 바로 false 반환
+            }
+        }
+
+        return true;  //끝까지 약수가 없으면 true 반환
     }
 }

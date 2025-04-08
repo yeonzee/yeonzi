fd4d5ea7727af7ba64a9f35dadaed94c461535d1
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-08 11:36:15 +0900

    [level 2] Title: 큰 수 만들기, Time: 8921.68 ms, Memory: 380 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -3,20 +3,23 @@ class Solution {
     public String solution(String number, int k) {
         String answer = "";
         
+        
         int start = 0;
+        //뽑아야 하는 자릿수 만큼 반복
         for(int i=0; i<number.length()-k; i++) {
             char max = '0';
             for(int j=start; j<i+k+1; j++) {
-                if(number.charAt(j) > max) {
+                //큰 수 찾기
+                if(max<number.charAt(j)) {
                     max = number.charAt(j);
                     start = j+1;
                 }
-                
-                //시간 효율성을 위해 가장 큰 수인 9면 스탑
-                if(number.charAt(j) == '9') {
+                //효율성을 위해 제일 큰 수((9)라면 끝내버리기
+                if(max == '9') {
                     break;
                 }
             }
+            
             answer += Character.toString(max);
         }
         return answer;

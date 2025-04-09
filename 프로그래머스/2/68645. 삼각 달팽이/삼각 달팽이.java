8fa05a711d256a21a445a52d5ce036823bdfd36a
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-09 12:21:42 +0900

    [level 2] Title: 삼각 달팽이, Time: 13.75 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,22 +1,20 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n) {
-        
+
         int[][] arr = new int[n][n];
-        
-        int num = 0;
         
         int x = -1;
         int y = 0;
         
+        int num = 1;
+        
         for(int i=0; i<n; i++) {
-            
             //아래
             if(i%3==0) {
                 for(int j=i; j<n; j++) {
                     x++;
-                    num++;
-                    arr[x][y] = num;
+                    arr[x][y] = num++;
                 }
             }
             
@@ -24,33 +22,33 @@ class Solution {
             else if(i%3==1) {
                 for(int j=i; j<n; j++) {
                     y++;
-                    num++;
-                    arr[x][y] = num;
+                    arr[x][y] = num++;
                 }
             }
             
-            //대각선 위
+            //왼쪽 대각선
             else if(i%3==2) {
                 for(int j=i; j<n; j++) {
                     x--;
                     y--;
-                    num++;
-                    arr[x][y] = num;
+                    arr[x][y] = num++;
                 }
             }
         }
-        
-        int[] answer = new int[num];
-        
+            
         //answer에 넣기
+        int[] answer = new int[(n*(n+1))/2];
         int idx = 0;
+        
         for(int i=0; i<n; i++) {
-            for(int j=0; j<=i; j++) {
-                answer[idx] = arr[i][j];
-                idx++;
+            for(int j=0; j<n; j++) {
+                //0이 아닐경우
+                if(arr[i][j] != 0) {
+                    answer[idx] = arr[i][j];
+                    idx++;
+                }
             }
         }
-        
         return answer;
     }
 }

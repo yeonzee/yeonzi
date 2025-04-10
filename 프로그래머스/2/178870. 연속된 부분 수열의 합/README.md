8023960647f13cf9da0e09309f2ca481f5baf663
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-10 11:27:49 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 21.60 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 0.02 ms
+메모리: 130 MB, 시간: 21.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 28일 03:07:29
+2025년 04월 10일 11:27:44
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,35 +1,47 @@
+//투포인터
+import java.util.*;
 class Solution {
-    public int[] solution(int[] sequence, int k) {        
-
-    	int left = 0;
-    	int right = 0;
-    	int sum = 0;
-    	int size = sequence.length;
-    	int ans1 = 0;
-    	int ans2 = 0;
-    	
-    	for(right=0; right<sequence.length; right++) {    		
-    		sum += sequence[right];
-    		
-    		while(sum > k) {
-    			sum -= sequence[left];
-    			left++;
-    		}
-    		
-    		if(sum == k) {
-    			if(size > right-left) {
-    				size = right-left;
-    				ans1 = left;
-    				ans2 = right;
-    			}
-    			else if(size == right-left) {
-    				ans1 = Math.min(ans1, left);
-    				ans2 = Math.min(ans2, right);
-    			}
-    		}
-    		
-    	}
-    	
-    	return new int[] {ans1, ans2};
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[2];
+        
+        int left = 0;
+        int sum = 0;
+        int max_len = sequence.length;
+        
+        int ans1 = 0;
+        int ans2 = 0;
+        
+        for(int right = 0; right<sequence.length; right++) {
+            sum += sequence[right];
+            
+            //합이 k를 넘어버리면 왼쪽(left)에서 하나씩 빼주기
+            if(sum > k) {
+                while(sum > k) {
+                    sum -= sequence[left];
+                    left++;
+                }
+            }
+            //k와 같다면
+            if(sum == k) {
+                //길이가 짧은 인덱스가 나와야 함
+                int len = right-left;
+                
+                if(len < max_len) {
+                    ans1 = left;
+                    ans2 = right;
+                    max_len = len;
+                }
+                //길이가 같다면 앞쪽의 인덱스가 나와야 함
+                else if(len == max_len) {
+                    ans1 = Math.min(ans1, left);
+                    ans2 = Math.min(ans2, right);
+                }
+            }
+        }
+        
+        answer[0] = ans1;
+        answer[1] = ans2;
+        
+        return answer;
     }
 }

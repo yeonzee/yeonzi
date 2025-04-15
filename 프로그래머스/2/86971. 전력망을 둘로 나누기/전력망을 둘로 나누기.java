dc79ff9f9a56c1be236d33a77ddc683801161b08
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-16 02:06:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.50 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -6,46 +6,48 @@ class Solution {
         int answer = n;
         map = new int[n+1][n+1];
         
-        //연결되어 있는 부분 1로 초기화
+        //연결되어 있는 부분들 1로 초기화
         for(int i=0; i<wires.length; i++) {
             map[wires[i][0]][wires[i][1]] = 1;
             map[wires[i][1]][wires[i][0]] = 1;
         }
         
-        //하나씩 끊어보기
+        //하나씩 끊어보면서 갯수 세기
         for(int i=0; i<wires.length; i++) {
+            //끊기
             map[wires[i][0]][wires[i][1]] = 0;
             map[wires[i][1]][wires[i][0]] = 0;
             
-            answer = Math.min(answer,bfs(n, wires[i][0]));  //wires[i][0] 만 넣는 이유: 자른 구간을 기준으로 한 구역만 계산
+            answer = Math.min(answer, bfs(wires[i][0], n, map));
             
-            //백트래킹
+            //끊은 전선 다시 되돌리기
             map[wires[i][0]][wires[i][1]] = 1;
             map[wires[i][1]][wires[i][0]] = 1;
+            
         }
-        
         return answer;
     }
     
-    //bfs에서 각 전력망들의 갯수 차이 구하기
-    public int bfs(int n, int start) {
-        //방문처리
+    public int bfs(int start, int n, int[][] map) {
+        Queue<Integer> q = new LinkedList<>();
+        
+        //시작 넣기
+        q.offer(start);
+        
+        //시작 방문처리
         boolean[] visited = new boolean[n+1];
-        
-        //시작하면서 나를 방문하기 때문에 1로 초기화
-        int cnt = 1;
-        
-        //나 자신 방문처리
         visited[start] = true;
         
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
+        //나 자신 방문하면서 시작하기 때문에
+        int cnt = 1;
         
         while(!q.isEmpty()) {
             int temp = q.poll();
             
             for(int i=1; i<n+1; i++) {
-                if(!visited[i] && map[temp][i] == 1) {  //갈 수 있음
+                //연결되어 있고 방문하지 않았다면 다시 탐방
+                if(map[temp][i] == 1 && !visited[i]) {
+                    //방문 처리
                     visited[i] = true;
                     cnt++;
                     q.offer(i);

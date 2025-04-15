e2fa1bca56c9d0177435c51e784d4f06e1c6b117
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-15 12:34:01 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 18.72 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -3,84 +3,73 @@ class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
         
-        //행렬 그리기(인덱스 1부터 시작)
-        int[][] map = new int[rows+1][columns+1];
-        int num=1;
-        for(int i=1; i<=rows; i++) {
-            for(int j=1; j<=columns; j++) {
+        int[][] map = new int[rows][columns];
+        int num = 1;
+        
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<columns; j++) {
                 map[i][j] = num;
                 num++;
             }
         }
         
         int idx = 0;
-        
-        for(int[] a:queries) {
-            //최솟값(가장 큰 값 넣어놓기)
+        for(int[] q:queries) {
+            int start_x = q[0]-1;
+            int start_y = q[1]-1;
+            int last_x = q[2]-1;
+            int last_y = q[3]-1;
+            
+            //이전 값
+            int temp1 = 0;
+            //현재 값
+            int temp2 = 0;
+            
+            //제일 작은 값
             int min = rows*columns;
             
-            //임시저장
-            int temp = map[a[0]][a[1]];
-            
-            int start_r = a[0];
-            int start_c = a[1];
-            int last_r = a[2];
-            int last_c = a[3];
-            
-            //왼쪽줄 회전
-            while(true) {
-                //마지막에 도달했으면 끝
-                if(start_r == last_r) {
-                    break;
-                }
-                map[start_r][start_c] = map[start_r+1][start_c];
-                start_r++;
-                
-                min = Math.min(min, map[start_r][start_c]);
+            //오른쪽
+            temp1 = map[start_x][start_y];
+            for(int i=start_y; i<last_y; i++) {
+                temp2 = map[start_x][i+1];
+                map[start_x][i+1] = temp1;
+                temp1 = temp2;
+                min = Math.min(min,temp2);
+            }
+    
+            //아래
+            for(int i=start_x; i<last_x; i++) {
+                temp2 = map[i+1][last_y];
+                map[i+1][last_y] = temp1;
+                temp1 = temp2;
+                min = Math.min(min,temp2);
             }
             
-            //아래줄 회전
-            while(true) {
-                //마지막에 도달했으면 끝
-                if(start_c == last_c) {
-                    break;
-                }
-                map[start_r][start_c] = map[start_r][start_c+1];
-                start_c++;
-                
-                min = Math.min(min, map[start_r][start_c]);
+            //왼쪽
+            for(int i=last_y; i>start_y; i--) {
+                temp2 = map[last_x][i-1];
+                map[last_x][i-1] = temp1;
+                temp1 = temp2;
+                min = Math.min(min,temp2);
             }
             
-            //오른쪽줄 회전
-            while(true) {
-                //마지막에 도달했으면 끝
-                if(start_r == a[0]) {
-                    break;
-                }
-                map[start_r][start_c] = map[start_r-1][start_c];
-                start_r--;
-                
-                min = Math.min(min, map[start_r][start_c]);
+            //위
+            for(int i=last_x; i>start_x; i--) {
+                temp2 = map[i-1][start_y];
+                map[i-1][start_y] = temp1;
+                temp1 = temp2;
+                min = Math.min(min,temp2);
             }
-            
-            //윗줄 회전
-            while(true) {
-                //마지막에 도달했으면 끝
-                if(start_c == a[1]) {
-                    break;
-                }
-                map[start_r][start_c] = map[start_r][start_c-1];
-                start_c--;
-                
-                min = Math.min(min, map[start_r][start_c]);
-            }
-            
-            //임시값 넣어주기
-            map[a[0]][a[1]+1] = temp;
-            min = Math.min(min, map[a[0]][a[1]+1]);
             
             answer[idx] = min;
             idx++;
+            
+            // for(int k=0; k<map.length; k++) {
+            //     for(int h=0; h<map[0].length; h++) {
+            //         System.out.print(map[k][h]);
+            //     }
+            //     System.out.println();
+            // }
         }
         return answer;
     }

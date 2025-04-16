9c6c7ed343a8300af10963d5747eaeb4030dcb83
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-16 10:52:32 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.97 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,9 +4,9 @@ class Solution {
     static int[][] map;
     public int solution(int n, int[][] wires) {
         int answer = n;
-        map = new int[n+1][n+1];
+        map = new int[n+1][n+1]; 
         
-        //연결되어 있는 부분들 1로 초기화
+        //연결되어 있는 부분 1로 설정하기
         for(int i=0; i<wires.length; i++) {
             map[wires[i][0]][wires[i][1]] = 1;
             map[wires[i][1]][wires[i][0]] = 1;
@@ -18,43 +18,38 @@ class Solution {
             map[wires[i][0]][wires[i][1]] = 0;
             map[wires[i][1]][wires[i][0]] = 0;
             
-            answer = Math.min(answer, bfs(wires[i][0], n, map));
+            answer = Math.min(answer, bfs(wires[i][0], n));
             
             //끊은 전선 다시 되돌리기
             map[wires[i][0]][wires[i][1]] = 1;
             map[wires[i][1]][wires[i][0]] = 1;
-            
         }
         return answer;
     }
     
-    public int bfs(int start, int n, int[][] map) {
+    public int bfs(int start, int n) {
         Queue<Integer> q = new LinkedList<>();
         
         //시작 넣기
         q.offer(start);
-        
-        //시작 방문처리
+        //시작 방문 처리
         boolean[] visited = new boolean[n+1];
         visited[start] = true;
-        
-        //나 자신 방문하면서 시작하기 때문에
+        //나를 방문하면서 시작하므로 1부터 시작
         int cnt = 1;
         
         while(!q.isEmpty()) {
             int temp = q.poll();
             
             for(int i=1; i<n+1; i++) {
-                //연결되어 있고 방문하지 않았다면 다시 탐방
+                //연결되어 있고 방문하지 않았다면 거기서부터 다시 탐색
                 if(map[temp][i] == 1 && !visited[i]) {
-                    //방문 처리
-                    visited[i] = true;
                     cnt++;
+                    visited[i] = true;
                     q.offer(i);
                 }
             }
         }
-        
         return Math.abs(n-2*cnt);
     }
 }

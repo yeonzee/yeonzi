0f85a922e4142ac420eecba5ddbbb6ebb822381f
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-17 10:51:58 +0900

    [level 2] Title: 배달, Time: 5.86 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,14 +1,14 @@
-//최단거리(한 정점에서만 최단거리를 구하므로) -> 다익스트라 알고리즘
+//최단거리 -> 다익스트라 알고즘
 import java.util.*;
 class Solution {
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
 
-        //거리에 최솟값을 넣기 위해 모든 거리를 최댓값으로 초기화해서 만들어준다
         int[][] map = new int[N][N];
+        //거리를 최솟값으로 갱신하기 위해 가장 큰 값 넣어주기
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
-                //나 자신은 0으로 초기화
+                //나 자신은 거리를 0으로 처리
                 if(i==j) {
                     map[i][j] = 0;
                 }
@@ -19,25 +19,25 @@ class Solution {
             }
         }
         
-        //road의 값 넣어준다
+        //road 값 넣어주기
         for(int[] r:road) {
-            //더 작은 수가 있으면 건너뛰기
+            //이미 더 작은 값이 들어가있다면 갱신하지 말고 넘어가기
             if(map[r[0]-1][r[1]-1] < r[2]) {
                 continue;
-            }            
+            }
             map[r[0]-1][r[1]-1] = r[2];
             map[r[1]-1][r[0]-1] = r[2];
         }
         
         //다익스트라 알고리즘
         
-        //중간에 경유
+        //경유
         for(int i=0; i<map.length; i++) {
             //시작
             for(int j=0; j<map.length; j++) {
                 //마지막
                 for(int k=0; k<map.length; k++) {
-                    //만약 중간에 경유하는 경우의 값이 더 작다면 갱신
+                    //경유하는 것이 더 작다면 그것으로 갱신
                     if(map[j][k] > map[j][i] + map[i][k]) {
                         map[j][k] = map[j][i] + map[i][k];
                     }
@@ -45,13 +45,13 @@ class Solution {
             }
         }
         
-        //K보다 작은 값 추출
+        //k보다 작은 것
         for(int i=0; i<map.length; i++) {
             if(map[0][i] <= K) {
                 answer++;
             }
         }
-
+        
         return answer;
     }
 }

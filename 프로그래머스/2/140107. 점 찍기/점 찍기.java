7da65b6ff61ffae4c7ba22bd4f392e85d436bcd2
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-21 02:33:32 +0900

    [level 2] Title: 점 찍기, Time: 14.85 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,22 +1,18 @@
-//i^2+j^2 <= d^2  ->  j^2 <= d^2-i^2
+//i*2 + j*2 <= d*2 -> j*2 <= d*2-i*2
 import java.util.*;
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
         
-        //효율성위해 for문 밖에서 계산
+        //거리
         long dis = (long)d*d;
         
         for(int i=0; i<=d; i+=k) {
-            //현재에서 갈 수 있는 거리
+            //i를 고정해놨을 때 갈 수 있는 거리
             long now_dis = (long)Math.sqrt(dis - (long)i*i);
             
-            //k간격만큼 멀어지므로 /k
-            //,0좌표 포함 +1
             answer += (now_dis/k)+1;
         }
         return answer;
     }
 }
-
-//long 변환 신경쓰기...!!!

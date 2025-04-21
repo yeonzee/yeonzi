5508dc8228c8315d864a123099aa1ac89355abc8
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-21 23:57:59 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,44 +1,38 @@
-//뒷자리부터 보기
-//0,1,2,3,4면 올림버튼
-//6,7,8,9면 내림버튼 (10에서 빼기) -> 바로 앞 숫자 +1
-//5면 앞의 숫자 확인
-
+/*
+storey = 현재 층 / 내가 가야하는 층: 0층
+뒤에서부터 보기
+0,1,2,3,4 -> - 버튼
+6,7,8,9 -> + 버튼
+5는 앞의 숫자와 합쳐서 판단
+*/
 import java.util.*;
 class Solution {
     public int solution(int storey) {
         int answer = 0;
         
         while(storey != 0) {
-            int num = storey % 10;
+            int n = storey % 10;
             storey /= 10;
             
-            if(num < 5) {
-                answer += num;
+            if(n < 5) {
+                answer += n;
             }
-            else if(num > 5) {
-                answer += (10-num);
-                storey++;  //내림버튼을 눌러야 하기 때문에 +1
+            else if(n > 5) {
+                answer += 10-n;
+                storey++;   //더 크게 내려가서 뒷 숫자만큼 올라와야 하기 때문에 +1
             }
-            else {  //5일 경우
-                if(storey % 10 >= 5) {  //앞의 숫자가 5보다 크거나 같다면 내림버튼 누르는 게 효율적
-                    answer += (10-num);
+            else if(n == 5) {
+                //5 앞의 수가 5보다 크거나 같을 경우 더 큰 숫자에서 올라와야 함
+                if(storey % 10 >= 5) {
+                    answer += 10-n;
                     storey++;
                 }
-                else {  //그 앞의 숫자가 5보다 작으면 올림버튼 누르는 게 효율적
-                    answer += num;
+                //5 앞의 수가 5보다 작은 경우 거기서 내려가야 함
+                else {
+                    answer += n;
                 }
             }
         }
-        
         return answer;
     }
 }
-
-//예시 -> 2554
-/* 4일 경우 올림 버튼 +4, storey = 255
-5이므로 앞의 숫자 확인 -> 55
-60 -> 55는 5+4 = 9이다. 50 -> 55는 5+5=10이다 
-그러므로 앞의 숫자를 올려서 내림버튼 누르는 게 최소값
-storey=26됨 내림버튼 +4, 앞의 층 +1
-결론: 4+5+4+3 = 16
-복잡하다 참...*/

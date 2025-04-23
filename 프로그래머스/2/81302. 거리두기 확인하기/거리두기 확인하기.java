b5a0616b78dd3796f6cb303f7b7ffac11a534faa
zoo <125468560+yeonzee@users.noreply.github.com>
2025-04-23 10:40:26 +0900

    [level 2] Title: 거리두기 확인하기, Time: 3.62 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -4,55 +4,69 @@ class Solution {
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
+        int[] answer = new int[places.length];
         
-        for(int i=0; i<places.length; i++) {
-            String[] a = places[i];
+        int idx = 0;
+        
+        for(String[] p:places) {
             boolean check = true;
-            for(int j=0; j<5; j++) {
-                for(int k=0; k<5; k++) {
-                    if(a[j].charAt(k) == 'P') {
-                        if(!bfs(a, j, k)) {
-                            //거리두기를 지키지 않은 경우
+            for(int i=0; i<5; i++) {
+                for(int j=0; j<5; j++) {
+                    //P라면
+                    if(p[i].charAt(j) == 'P') {
+                        //한 부분이라도 거리두기를 지키지 못했다면 0
+                        if(!bfs(i,j,p)) {
                             check = false;
+                            break;
                         }
                     }
                 }
             }
-            answer[i] = check? 1:0;
+            //거리두기를 지켰다면
+            if(check) {
+                answer[idx] = 1;
+            }
+            else {
+                answer[idx] = 0;
+            }
+            idx++;
         }
         return answer;
     }
-    public boolean bfs(String[] a, int x, int y) {
+    
+    public boolean bfs(int x, int y, String[] p) {
+        
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x,y});
+        q.offer(new int[]{x,y});
         
         while(!q.isEmpty()) {
             int[] temp = q.poll();
+            int now_x = temp[0];
+            int now_y = temp[1];
             
             for(int i=0; i<4; i++) {
-                int nx = temp[0]+dx[i];
-                int ny = temp[1]+dy[i];
+                int next_x = now_x + dx[i];
+                int next_y = now_y + dy[i];
                 
-                //탐색 범위 벗어남 + 나 자신 무시
-                if(nx<0 || ny<0 || nx>=5 || ny>=5 || (nx==x && ny==y)) {
+                //범위를 벗어나거나 나 자신 패스
+                if(next_x<0 || next_x>=5 || next_y<0 || next_y>=5 || (next_x==x && next_y==y)) {
                     continue;
                 }
                 
                 //맨해튼 거리
-                int m = Math.abs(nx-x) + Math.abs(ny-y);
+                int m = Math.abs(next_x-x) + Math.abs(next_y-y);
                 
-                //이동한 곳이 P이고 맨해튼 거리가 2보다 작거나 같으면 거리두기 실패
-                if(a[nx].charAt(ny) == 'P' && m<=2) {
+                //맨해튼 거리가 2보다 작고 이동한 곳에 다른 응시자가 있으면 거리두기 실패
+                if(m <= 2 && p[next_x].charAt(next_y) == 'P') {
                     return false;
                 }
-                //이동한 곳이 빈자리이고 맨해튼 거리가 2보다 작으면 그 곳을 기준으로 P가 있는지 재탐색
-                if(a[nx].charAt(ny) == 'O' && m<2) {
-                    q.add(new int[]{nx,ny});
+                //맨해튼 거리가 2보다 작고 이동한 곳이 빈자리이면 거기서부터 재탐색
+                if(m < 2 && p[next_x].charAt(next_y) == 'O') {
+                    q.add(new int[]{next_x, next_y});
                 }
+                
             }
         }
-        
         //거리두기 지킴
         return true;
     }

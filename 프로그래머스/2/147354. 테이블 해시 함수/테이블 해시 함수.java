23a81ca7a36352fd31987149655d8865e5e4cd84
zoo <125468560+yeonzee@users.noreply.github.com>
2025-05-01 00:08:47 +0900

    [level 2] Title: 테이블 해시 함수, Time: 11.09 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,18 +1,23 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
-        Arrays.sort(data,(o1, o2) -> o1[col-1]!=o2[col-1] ?o1[col-1]-o2[col-1] :o2[0]-o1[0]);
-        for(int i=row_begin-1;i<row_end;i++){
-            int si = 0;
-            for(int j=0; j<data[i].length;j++){
+        
+        //정렬
+        //col을 중심으로 오름차순
+        //숫자가 같다면 첫번째 숫자를 기준으로 내림차순
+        Arrays.sort(data, (o1,o2) ->
+                   o1[col-1] != o2[col-1] ? o1[col-1]-o2[col-1] : o2[0]-o1[0]);
+        
+        for(int i=row_begin-1; i<row_end; i++) {
+            int si=0;
+            for(int j=0; j<data[i].length; j++) {
                 si += data[i][j] % (i+1);
             }
-            answer = answer^si;
-
+            
+            answer = answer ^ si;
         }
-
+        
         return answer;
     }
 }

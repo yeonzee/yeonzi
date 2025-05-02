508c50f0a14576c934df1c66548d53eaed1dd23a
zoo <125468560+yeonzee@users.noreply.github.com>
2025-05-02 11:54:06 +0900

    [level 2] Title: 하노이의 탑, Time: 5.39 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,27 +1,29 @@
 import java.util.*;
-
 class Solution {
-    private static List<int[]> arr = new ArrayList<>();
-
+    static ArrayList<int[]> arr = new ArrayList<>();
     public int[][] solution(int n) {
-        move(n, 1, 2, 3);
-        // int[][] answer = arr.stream()
-        //         .toArray(int[][]::new);
-
-        int[][] answer = new int[arr.size()][];
-        for (int i = 0; i < arr.size(); i++) {
+        //시작
+        move(n,1,2,3);
+        
+        int[][] answer = new int[arr.size()][2];
+        for(int i=0; i<arr.size(); i++) {
             answer[i] = arr.get(i);
         }
-        
         return answer;
     }
-
-    private static void move(int cnt, int start, int mid, int end) {
-        if (cnt == 0) {
+    
+    //재귀
+    public void move(int n, int start, int mid, int end) {
+        //종료조건
+        if(n==0) {
             return;
         }
-        move(cnt - 1, start, end, mid);
+        
+        //마지막 원판 빼고 중간으로 몰기
+        move(n-1,start,end,mid);
+        //list에 담기
         arr.add(new int[]{start, end});
-        move(cnt - 1, mid, start, end);
+        //중간에서 끝으로 보내기
+        move(n-1,mid,start,end);
     }
 }

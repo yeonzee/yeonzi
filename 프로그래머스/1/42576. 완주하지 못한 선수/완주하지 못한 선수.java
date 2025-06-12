5219b89efabad2150316ac48d5c26e27dac90b59
zoo <125468560+yeonzee@users.noreply.github.com>
2025-06-12 23:09:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 102.92 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,26 @@
-import java.util.HashMap;
-
+import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        
         HashMap<String, Integer> map = new HashMap<>();
         
-        // participant map에 넣기
-        for (String s : participant) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+        //참여자 다 넣기
+        for(String s:participant) {
+            map.put(s, map.getOrDefault(s,0)+1);
         }
         
-        // completion에 있는 선수 개수 빼기
-        for (String s : completion) {
-            map.put(s, map.get(s) - 1);
+        //완주자 빼기
+        for(String s:completion) {
+            map.put(s, map.getOrDefault(s,0)-1);
         }
         
-        // 완주하지 못한 참가자 
-        for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
-                return key;
+        //0 이상이라면 완주하지 못했다는 뜻
+        for(String k:map.keySet()) {
+            if(map.get(k) > 0) {
+                answer = k;
             }
         }
-        
-        return answer; 
+        return answer;
     }
 }

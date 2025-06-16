b3ad834aa2d28b0380ea34636d344880575e5c11
zoo <125468560+yeonzee@users.noreply.github.com>
2025-06-17 01:43:16 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -6,14 +6,20 @@ class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         
         for(String[] temp:clothes) {
-            map.put(temp[1], map.getOrDefault(temp[1],0)+1);  
+            map.put(temp[1], map.getOrDefault(temp[1],0)+1);
         }
         
-        for(String str:map.keySet()) {
-            answer *= map.get(str)+1;
+        for(String key:map.keySet()) {
+            answer *= map.get(key)+1;
         }
         
-        //아무것도 안입는 경우 제외
         return answer-1;
     }
 }
+
+/*
+y_h, g_t, x
+b_s, x
+-> 6가지 조합 나옴
+-> 아무것도 안입는 경우(x,x) 제외
+*/

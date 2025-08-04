74705ac23b7ad90575edaf3922969018e82e4d9f
zoo <125468560+yeonzee@users.noreply.github.com>
2025-08-04 23:54:59 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.23 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,15 +2,18 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] str = s.split(" ");
-        int[] num = new int[str.length];
+        String[] temp = s.split(" ");
+        int[] num = new int[temp.length];
         
-        for(int i=0; i<str.length; i++) {
-            num[i] = Integer.parseInt(str[i]);
-        }
+        for(int i=0; i<temp.length; i++) {
+            num[i] = Integer.parseInt(temp[i]);
+        } 
         
+        //정렬
         Arrays.sort(num);
+        
         answer = String.valueOf(num[0]) + " " + String.valueOf(num[num.length-1]);
+        
         return answer;
     }
 }

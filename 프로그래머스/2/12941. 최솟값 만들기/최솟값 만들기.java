c254b2e468113a9cf7ae6bf63e55466832325a21
zoo <125468560+yeonzee@users.noreply.github.com>
2025-08-15 14:28:21 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.52 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,13 +1,14 @@
+//A는 오름차순, B는 내림차순으로 해서 곱하기
 import java.util.*;
 class Solution {
     public int solution(int []A, int []B) {
         int answer = 0;
-
-        Arrays.sort(A);  //오름차순
-        Arrays.sort(B);  
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
         
         for(int i=0; i<A.length; i++) {
-            answer += A[i] * B[B.length-i-1];
+            answer += A[i] * B[A.length-i-1];
         }
 
         return answer;

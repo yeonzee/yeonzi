deaf801bf272b8aae598ad7921be8a682d0471ae
zoo <125468560+yeonzee@users.noreply.github.com>
2025-08-20 02:56:49 +0900

    [level 2] Title: 카펫, Time: 9.28 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,23 @@
-//시간 효율성을 위해 for문은 1개만
 import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        for(int i=3; i<brown; i++) {
-            
-            //나누어 딱 떨어지는 것만 계산
-            if((brown+yellow)%i==0) {
-                int j=(brown+yellow)/i;
+        for(int i=1; i<=yellow; i++) {
+            //딱 나누어 떨어질때만
+            if(yellow%i == 0) {
+                int j = yellow/i;
                 
-                if((i-2)*2+(j-2)*2+4 == brown) {
-                answer[0] = Math.max(i,j);
-                answer[1] = Math.min(i,j);
-            }
+                if(i*2 + j*2 + 4 == brown) {
+                answer[0] = i+2;
+                answer[1] = j+2;
+                }
             }
             
             
         }
+        
+        
         return answer;
     }
 }

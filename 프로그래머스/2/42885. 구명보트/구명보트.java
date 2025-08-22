3d1feb855e3cd983e31120e331478836b01d3a61
zoo <125468560+yeonzee@users.noreply.github.com>
2025-08-23 00:46:39 +0900

    [level 2] Title: 구명보트, Time: 10.37 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,19 +1,19 @@
-//정렬한 후 앞 뒤로 구명보트에 담기
-//문제를 제대로 읽자 한 번에 최대 2명 탈 수 있음
 import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
+        
         Arrays.sort(people);
-    
+        
         int i = 0;
         int j = people.length-1;
         
         while(true) {
+            
             //종료조건
             if(i>j) {
                 break;
-            }
+            } 
             
             int sum = people[i] + people[j];
             

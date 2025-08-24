994addf4201cada66b4f5aa6f42a762fa1c9ff7f
zoo <125468560+yeonzee@users.noreply.github.com>
2025-08-24 19:00:32 +0900

    [level 2] Title: 귤 고르기, Time: 85.44 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,25 +1,30 @@
+//map에 넣은 후 많은 순부터 빼기?
 import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        
         HashMap<Integer, Integer> map = new HashMap<>();
         
-        for(int num:tangerine) {
-            map.put(num, map.getOrDefault(num,0)+1);
+        for(int t:tangerine) {
+            map.put(t,map.getOrDefault(t,0)+1);
         }
         
-        //value기준 내림차순 정렬
-        //map은 순서를 보장하지 않는 자료구조이다
-        //list로 변환하여 정렬해야 함
-        ArrayList<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, (o1,o2) -> map.get(o2)-map.get(o1));
+        //System.out.print(map);
         
-        int sum = 0;
+        //정렬
+        //map으로 정렬할 수 없으므로 list로 변환한다
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1));
+        
+        //System.out.print(list);
+        
+        int sum=0;
+        
         for(int i=0; i<list.size(); i++) {
             sum += map.get(list.get(i));
-            answer++;
+            answer++; 
             
+            //종료조건
             if(sum >= k) {
                 break;
             }

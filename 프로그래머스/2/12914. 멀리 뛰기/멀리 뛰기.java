d43c54da69af380c8cc111941131c8ac1621b206
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-02 23:42:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.16 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,21 +1,19 @@
 //2->2, 3->3, 4->5, 5->8
-//피보나치
-//재귀로 구현하면 시간 초과
+//피보나치 -> 재귀로 구현하면 시간초과임
 import java.util.*;
 class Solution {
     public long solution(int n) {
         long answer = 0;
         
-        //n+1했더니 런타임오류가 뜸 -> 메모리 최적화 오류 발생...?
-        long[] a = new long[2001];
+        long[] fibo = new long[n+1];
         
-        a[1] = 1;
-        a[2] = 2;
+        fibo[0] = 1;
+        fibo[1] = 1;
         
-        for(int i=3; i<=n; i++) {
-            a[i] = (a[i-1] + a[i-2])%1234567;
+        for(int i=2; i<=n; i++) {
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
         }
         
-        return a[n];
+        return fibo[n];
     }
 }

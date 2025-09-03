61d575926927f1f6de140aedb05a11a2157f3928
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-03 22:03:41 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981#qna) 
 
 ### 성능 요약
 
-메모리: 84.8 MB, 시간: 0.20 ms
+메모리: 75.7 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 12일 22:25:31
+2025년 09월 03일 22:03:35
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,30 +1,26 @@
+//조건: 마지막 알파벳으로 시작 + 이전에 나오지 않았던 단어
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
         
-        //이전의 값을 담을 리스트
-        ArrayList<String> list = new ArrayList<>();
-        list.add(words[0]);
-        
-        for(int i=1; i<words.length; i++) {
+        for(int i=0; i<words.length-1; i++) {
+            String now = words[i];
+            String next = words[i+1];
             
-            //규칙에 어긋나는지 확인
-            if(words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)) {
-                answer[0] = (i%n)+1;
-                answer[1] = (i/n)+1;
-                break;
+            //끝말잇기 실패
+            if(now.charAt(now.length()-1) != next.charAt(0)) {
+                return new int[]{ (i+1) % n + 1, (i+1) / n + 1 };
             }
             
-            //이전에 등장한 단어인지 확인
-            if(list.contains(words[i])) {
-                answer[0] = (i%n)+1;
-                answer[1] = (i/n)+1;
-                break;
+            //이미 나온 단어면 실패
+            for(int j=0; j<=i; j++) {
+                if(next.equals(words[j])) {
+                    return new int[]{ (i+1) % n + 1, (i+1) / n + 1 };
+                }
             }
-            
-            list.add(words[i]);
         }
+
         
         return answer;
     }

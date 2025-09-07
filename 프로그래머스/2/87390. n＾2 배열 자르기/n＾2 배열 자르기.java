d3e5b7ba2d138c7d73cd27c371b18e7aaa24ba8f
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-07 19:17:39 +0900

    [level 2] Title: n^2 배열 자르기, Time: 8.63 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,18 +1,36 @@
-//내가 푼 거
 import java.util.*;
 class Solution {
     public int[] solution(int n, long left, long right) {
-        int length = (int)(right - left) + 1;
-        int[] answer = new int[length];
+        int len = (int)(right-left) + 1;
         
-        for (int i=0 ; i<length ; i++) {
-            long index = left + i;
-            int a = (int)(index / n);
-            int b = (int)(index % n);
+        int[] answer = new int[len];
+        
+        for(int i=0; i<len; i++) {
+            //인덱스
+            long index = i+left;
+            //몫
+            int a = (int)(index/n);
+            //나머지
+            int b = (int)(index%n);
             
-            answer[i] = Math.max(a,b) + 1;
+            //둘 중에 큰 수에 +1
+            answer[i] = Math.max(a,b)+1;
         }
         
         return answer;
     }
 }
+
+/*
+index 0 → (0,0) → max(0,0)+1 = 1
+index 1 → (0,1) → max(0,1)+1 = 2
+index 2 → (0,2) → max(0,2)+1 = 3
+
+index 3 → (1,0) → max(1,0)+1 = 2
+index 4 → (1,1) → max(1,1)+1 = 2
+index 5 → (1,2) → max(1,2)+1 = 3
+
+index 6 → (2,0) → max(2,0)+1 = 3
+index 7 → (2,1) → max(2,1)+1 = 3
+index 8 → (2,2) → max(2,2)+1 = 3
+*/

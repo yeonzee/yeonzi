d8c5a0e410ff7fd9ade823c5e12b86410a08a95c
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-07 18:49:11 +0900

    [level 2] Title: 괄호 회전하기, Time: 111.65 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,45 +1,43 @@
-//스택
 import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        
+
         for(int i=0; i<s.length(); i++) {
-            String str = s.substring(i) + s.substring(0,i);
+            Stack<Character> stack = new Stack<>(); //초기화
             
             //시작이 닫힌 괄호이면 x
-            if(str.charAt(0) == ']' || str.charAt(0) == ')' || str.charAt(0) == '}') {
+            if(s.charAt(i) == ']' || s.charAt(i) == ')' || s.charAt(i) == '}') {
                 continue;
             }
             
-            Stack<Character> stack = new Stack<>();
-            stack.push(str.charAt(0));
-            
-            for(int j=1; j<str.length(); j++) {
-                
-                //열린 괄호
-                if(str.charAt(j) == '(' || str.charAt(j) == '[' || str.charAt(j) == '{') {
-                    stack.push(str.charAt(j));
+            for(int j=i; j<s.length()+i; j++) {
+                //열림이라면 스택에 넣고 다음 확인하기
+                if(s.charAt(j%s.length()) == '(' || s.charAt(j%s.length()) == '[' || s.charAt(j%s.length()) == '{') {
+                    stack.push(s.charAt(j%s.length()));
                 }
-                
-                //올바른 괄호
-                else if(!stack.isEmpty() && stack.peek() == '(' && str.charAt(j) == ')') {
-                   stack.pop(); 
-                }
-                else if(!stack.isEmpty() && stack.peek() == '{' && str.charAt(j) == '}') {
-                   stack.pop(); 
-                }
-                else if(!stack.isEmpty() && stack.peek() == '[' && str.charAt(j) == ']') {
-                   stack.pop(); 
+
+                //닫힘이라면 스택에 괄호 쌍이 들어가 있는지 확인
+                else {
+                    //쌍에 맞는 괄호인지 확인
+                    if(!stack.isEmpty() && stack.peek() == '(' && s.charAt(j%s.length()) == ')') {
+                        stack.pop();
+                    }
+                    else if(!stack.isEmpty() && stack.peek() == '[' && s.charAt(j%s.length()) == ']') {
+                        stack.pop();
+                    }
+                    else if(!stack.isEmpty() && stack.peek() == '{' && s.charAt(j%s.length()) == '}') {
+                        stack.pop();
+                    }
                 }
             }
-            
-            //stack이 비어있다면 올바른 괄호 문자열
+            //스택이 비어있다면 올바른 괄호 문자열
             if(stack.isEmpty()) {
                 answer++;
             }
         }
-        
         return answer;
+
     }
+
 }

e93c4d3b90a9fd40fb27a8f29fb8073a90491ac3
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-11 22:50:57 +0900

    [level 2] Title: 전화번호 목록, Time: 333.91 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,16 +3,15 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        //앞의 숫자를 기준으로 정렬
+        //오름차순 정렬
         Arrays.sort(phone_book);
         
-        //시작하는지 판별 = startsWith
         for(int i=0; i<phone_book.length-1; i++) {
             if(phone_book[i+1].startsWith(phone_book[i])) {
                 return false;
             }
         }
-
-        return answer;
+        
+        return true;
     }
 }

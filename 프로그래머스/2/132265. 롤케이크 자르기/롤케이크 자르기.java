7b1dff61fb93194dbf9bb1bd765334b251133167
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-13 02:29:07 +0900

    [level 2] Title: 롤케이크 자르기, Time: 215.39 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,37 +1,35 @@
-//이중 for문을 활용한 set은 시간 초과
+//for문을 이용한 set은 시간초과
 //map 사용
 import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
         
-        //형 토핑 map
+        //형
         HashMap<Integer, Integer> map_1 = new HashMap<>();
         
-        //동생 토핑 map
+        //동생
         HashMap<Integer, Integer> map_2 = new HashMap<>();
         
-        //동생한테 몰아주기
-        for(int n:topping) {
-            map_2.put(n,map_2.getOrDefault(n,0)+1);
+        //형한테 몰빵
+        for(int t:topping) {
+            map_1.put(t, map_1.getOrDefault(t,0)+1);
         }
         
-        //형이 하나씩 뺏기
-        for(int n:topping) {
+        //동생이 하나씩 뺏기
+        for(int t:topping) {
             
-            //동생한테 1개 남은 걸 형한테 주면 동생한테서 토핑 제거
-            if(map_2.get(n)==1) {
-                map_2.remove(n);
+            //만약 1개 남은걸 준다면 형한테서 해당 토핑을 사라짐
+            if(map_1.get(t) == 1) {
+                map_2.put(t, map_2.getOrDefault(t,0)+1);
+                map_1.remove(t);
             }
             else {
-                //동생 토핑 뺏기
-                map_2.put(n,map_2.getOrDefault(n,0)-1);
+                map_2.put(t, map_2.getOrDefault(t,0)+1);    //동생한테 추가
+                map_1.put(t, map_1.getOrDefault(t,0)-1);    //형한테서 빼기
             }
             
-            //형한테 토핑 추가
-            map_1.put(n,map_1.getOrDefault(n,0)+1);
-            
-            //형이랑 동생이랑 토핑 갯수가 같다면
+            //형과 동생의 토핑 갯수가 같아진다면
             if(map_1.size() == map_2.size()) {
                 answer++;
             }

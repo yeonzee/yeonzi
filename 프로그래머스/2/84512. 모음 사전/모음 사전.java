f470141e473ce1d27c06ce69b76c1afa532c4802
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-17 22:07:27 +0900

    [level 2] Title: 모음 사전, Time: 10.40 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,35 +1,30 @@
 import java.util.*;
+
 class Solution {
-    static String[] alpha = {"A","E","I","O","U"};
-    static int cnt = 0;
-    static int answer = 0;
+    static String[] alpha;
+    static int answer;
     static boolean fin = false;
+
     public int solution(String word) {
-        
-        dfs(word, "", 0);
-        
+        answer = 0;
+        fin = false;
+        alpha = new String[]{"A","E","I","O","U"};
+        dfs(0, word, "");
         return answer;
     }
-    
-    public void dfs(String word, String w, int depth) {
-        //효율성을 위해 추가적인 재귀함수 호출x
-        if(fin) {
-            return;
-        }
-        
-        if(w.equals(word)) {
-            answer = cnt;
+
+    public void dfs(int depth, String word, String w) {
+        if (fin) return;                 // 이미 찾았으면 더 진행 X
+        if (w.equals(word)) {            // 정답 발견
             fin = true;
             return;
         }
-        
-        if(depth == 5) {
-            return;
-        }
-        
-        for(int i=0; i<alpha.length; i++) {
-            cnt++;
-            dfs(word, w+alpha[i], depth+1);
+        if (depth == 5) return;          // 길이 5 제한
+
+        for (int i = 0; i < alpha.length; i++) {
+            if (fin) return;             // ★ 루프 도중에도 즉시 중단
+            answer++;                    // 방문(생성)하는 단어 개수 카운트
+            dfs(depth + 1, word, w + alpha[i]);
         }
     }
 }

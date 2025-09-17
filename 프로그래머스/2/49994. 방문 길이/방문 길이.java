20cb81dec40a8cd39daa7180a374d640ea5a2270
zoo <125468560+yeonzee@users.noreply.github.com>
2025-09-17 23:11:39 +0900

    [level 2] Title: 방문 길이, Time: 19.53 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,18 +2,22 @@ import java.util.*;
 class Solution {
     public int solution(String dirs) {
         int answer = 0;
-        //중복을 없애기 위해 set 사용
+        
+        //중복을 방지하기 위해 set 사용
         HashSet<String> set = new HashSet<>();
         
-        
-        //현재 값 초기화 (0,0)
+        //0으로 초기화
         int nowx = 0;
         int nowy = 0;
         
-        for(int i=0;i<dirs.length();i++) {
-            int nextx = nowx;
-            int nexty = nowy;
+        int nextx = 0;
+        int nexty = 0;
+        
+        for(int i=0; i<dirs.length(); i++) {
+            
             String road = "";
+            
+            //U일 경우 y++
             if(dirs.charAt(i) == 'U') {
                 nexty++;
                 road += nowx;
@@ -22,7 +26,7 @@ class Solution {
                 road += nexty;
             }
             
-            //U,D일 경우 road에 반대로 저장하는 이유: 되돌아 가는 경우도 같은 경로로 치기 위해
+            //D일 경우 U의 역방향
             else if(dirs.charAt(i) == 'D') {
                 nexty--;
                 road += nextx;
@@ -31,6 +35,7 @@ class Solution {
                 road += nowy;
             }
             
+            //R일 경우 x++
             else if(dirs.charAt(i) == 'R') {
                 nextx++;
                 road += nowx;
@@ -39,7 +44,8 @@ class Solution {
                 road += nexty;
             }
             
-            else if(dirs.charAt(i) == 'L') {
+            //L일 경우 R의 역방향
+            else {
                 nextx--;
                 road += nextx;
                 road += nexty;
@@ -47,17 +53,34 @@ class Solution {
                 road += nowy;
             }
             
-            //범위를 벗어나면 set에 저장하지 않는다
-            if (nextx > 5 || nextx < -5 || nexty > 5 || nexty < -5) {
+            //범위를 벗어나는 경우는 패스
+            if(nextx<-5 || nextx > 5|| nexty<-5 || nexty>5) {
+                //범위를 벗어난경우 continue하기 때문에 안 간 것으로 취급해야 함.
+                nextx = nowx;
+                nexty = nowy;
                 continue;
             }
             
             set.add(road);
             
+            //갱신
             nowx = nextx;
             nowy = nexty;
+            
         }
         
         return set.size();
     }
 }
+
+/*
+지금 내가 있는 위치: nowx, nowy
+내가 움직인 위치: nextx, nexty
+U&D로 예를 들어 보자면,
+0,0 -> U -> 0,1이 된다
+만약 거기서 D가 나온다면
+0,1 -> 0,0이 된다.
+이 길은 중복이기 때문에 같은 의미를 나타내어야 한다.
+그러므오 D는 U의 역행으로 String을 만들었다.
+L과 R도 마찬가지!
+*/
